025fdb9df1cb32a568c89898675210ef1e953043
Dan Nixon <dan@dan-nixon.com>
2022-09-21 13:45:46 +0100

    Add UTC timezoned conversions for GPS timestamp

--- streaming-types/src/util.rs
+++ streaming-types/src/util.rs
@@ -1,5 +1,5 @@
 use crate::status_packet_v1_generated::GpsTime;
-use chrono::{Datelike, NaiveDate, NaiveDateTime, Timelike};
+use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, Timelike, Utc};
 
 impl From<GpsTime> for NaiveDateTime {
     fn from(t: GpsTime) -> Self {
@@ -16,8 +16,41 @@ impl From<GpsTime> for NaiveDateTime {
     }
 }
 
+impl From<GpsTime> for DateTime<Utc> {
+    fn from(t: GpsTime) -> Self {
+        let nanosecond = (t.millisecond() as u32 * 1_000_000)
+            + (t.microsecond() as u32 * 1_000)
+            + (t.nanosecond() as u32);
+
+        NaiveDate::from_yo(2000 + (t.year() as i32), t.day().into())
+            .and_hms_nano(
+                t.hour().into(),
+                t.minute().into(),
+                t.second().into(),
+                nanosecond,
+            )
+            .and_local_timezone(Utc)
+            .unwrap()
+    }
+}
+
 impl From<NaiveDateTime> for GpsTime {
     fn from(t: NaiveDateTime) -> Self {
+        Self::new(
+            (t.year() - 2000) as u8,
+            t.ordinal() as u16,
+            t.hour() as u8,
+            t.minute() as u8,
+            t.second() as u8,
+            (t.nanosecond() / 1_000_000) as u16,
+            ((t.nanosecond() % 1_000_000) / 1_000) as u16,
+            (t.nanosecond() % 1_000) as u16,
+        )
+    }
+}
+
+impl From<DateTime<Utc>> for GpsTime {
+    fn from(t: DateTime<Utc>) -> Self {
         Self::new(
             (t.year() - 2000) as u8,
             t.ordinal() as u16,
@@ -53,8 +86,37 @@ mod tests {
     }
 
     #[test]
+    fn test_gpstime_to_datetimeutc() {
+        let t1 = GpsTime::new(22, 205, 14, 52, 22, 100, 200, 300);
+
+        let t2: DateTime<Utc> = t1.into();
+
+        assert_eq!(t2.year(), 2022);
+        assert_eq!(t2.month(), 07);
+        assert_eq!(t2.day(), 24);
+
+        assert_eq!(t2.hour(), 14);
+        assert_eq!(t2.minute(), 52);
+        assert_eq!(t2.second(), 22);
+
+        assert_eq!(t2.nanosecond(), 100200300);
+    }
+
+    #[test]
     fn test_naivedatetime_to_gpstime() {
         let t1 = NaiveDate::from_ymd(2022, 07, 24).and_hms_nano(14, 52, 22, 100200300);
+
+        let t2: GpsTime = t1.into();
+
+        assert_eq!(t2, GpsTime::new(22, 205, 14, 52, 22, 100, 200, 300));
+    }
+
+    #[test]
+    fn test_datetimeutc_to_gpstime() {
+        let t1 = NaiveDate::from_ymd(2022, 07, 24)
+            .and_hms_nano(14, 52, 22, 100200300)
+            .and_local_timezone(Utc)
+            .unwrap();
 
         let t2: GpsTime = t1.into();
 
