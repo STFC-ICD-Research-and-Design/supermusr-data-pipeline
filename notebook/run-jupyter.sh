93a3e3e87b7e3cd30991c97c40e1c8f36f6a3890
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-11-11 10:17:30 +0000

    [ci skip] Make script downloadable-executable

--- notebook/run-jupyter.sh
+++ notebook/run-jupyter.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# wget https://raw.githubusercontent.com/STFC-ICD-Research-and-Design/supermusr-data-pipeline/main/notebook/supermusr-plotting-frames.ipynb -O /tmp/supermusr-notebook/supermusr-plotting-frames.ipynb
+wget -x https://raw.githubusercontent.com/Pasarus/supermusr-data-pipeline/add-notebook/notebook/supermusr-plotting-frames.ipynb -O /tmp/supermusr-notebook/supermusr-plotting-frames.ipynb
+
+# shellcheck disable=SC2164
+cd /tmp/supermusr-notebook/
+
 conda_env=SUPERMUSRENV
 conda_exe=$(which conda)
 conda_install_dir=$(dirname "$(dirname "$conda_exe")")
