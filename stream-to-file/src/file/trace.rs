2803392346d699b484ed188e4cfac5ad9cbc2b53
Dan Nixon <dan@dan-nixon.com>
2022-11-11 11:26:35 +0000

    Store sample rate in trace file
    Fixes #31

--- stream-to-file/src/file/trace.rs
+++ stream-to-file/src/file/trace.rs
@@ -1,5 +1,5 @@
 use super::base::BaseFile;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use common::{channel_index, Intensity, CHANNELS_PER_DIGITIZER};
 use hdf5::Dataset;
 use ndarray::{s, Array, Array1};
@@ -9,6 +9,7 @@ use streaming_types::dat1_digitizer_analog_trace_v1_generated::DigitizerAnalogTr
 
 pub(crate) struct TraceFile {
     base: BaseFile,
+    sample_rate: Dataset,
     detector_data: Dataset,
     det_data_extents: Array1<usize>,
 }
@@ -19,6 +20,9 @@ impl TraceFile {
 
         let channel_count = digitizer_count * CHANNELS_PER_DIGITIZER;
 
+        let sample_rate = base.file.new_dataset::<u64>().create("sample_rate")?;
+        sample_rate.write_scalar(&0)?;
+
         let detector_data = base
             .file
             .new_dataset::<Intensity>()
@@ -27,12 +31,24 @@ impl TraceFile {
 
         Ok(TraceFile {
             base,
+            sample_rate,
             detector_data,
             det_data_extents: Array1::zeros((digitizer_count,)),
         })
     }
 
     pub(crate) fn push(&mut self, data: &DigitizerAnalogTraceMessage) -> Result<()> {
+        let old_sample_rate = self.sample_rate.read_scalar::<u64>()?;
+        if old_sample_rate > 0 && old_sample_rate != data.sample_rate() {
+            return Err(anyhow!(
+                "Sample rate has changed (old={}, new={})",
+                old_sample_rate,
+                data.sample_rate()
+            ));
+        } else if old_sample_rate == 0 {
+            self.sample_rate.write_scalar(&data.sample_rate())?;
+        }
+
         let old_det_data_shape = self.detector_data.shape();
 
         let frame_det_data_start_idx = self.det_data_extents[data.digitizer_id() as usize];
@@ -147,7 +163,7 @@ mod tests {
         let message = DigitizerAnalogTraceMessageArgs {
             digitizer_id,
             metadata: Some(metadata),
-            sample_rate: 0,
+            sample_rate: 1_000_000_000,
             channels: Some(fbb.create_vector(&[channel0, channel1])),
         };
         let message = DigitizerAnalogTraceMessage::create(&mut fbb, &message);
@@ -197,6 +213,9 @@ mod tests {
         );
 
         let file = file.base.file;
+
+        let sample_rate = file.dataset("sample_rate").unwrap();
+        assert_eq!(sample_rate.read_scalar::<u64>().unwrap(), 1_000_000_000);
 
         let detector_data = file.dataset("detector_data").unwrap();
         assert_eq!(detector_data.shape(), vec![num_channels, num_measurements]);
