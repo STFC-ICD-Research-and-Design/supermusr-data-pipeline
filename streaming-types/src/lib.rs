2fbad8d27060d99b631a72e6c3ca181dcfb8bb02
Dan Nixon <dan@dan-nixon.com>
2023-02-09 09:45:17 +0000

    Ignore more warnings from flatbuffer generated code

--- streaming-types/src/lib.rs
+++ streaming-types/src/lib.rs
@@ -1,19 +1,19 @@
 pub mod util;
 
 #[rustfmt::skip]
-#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc)]
+#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 pub mod aev1_frame_assembled_event_v1_generated;
 
 #[rustfmt::skip]
-#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc)]
+#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 pub mod dat1_digitizer_analog_trace_v1_generated;
 
 #[rustfmt::skip]
-#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc)]
+#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 pub mod dev1_digitizer_event_v1_generated;
 
 #[rustfmt::skip]
-#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc)]
+#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::size_of_in_element_count, clippy::missing_safety_doc, clippy::needless_lifetimes)]
 pub mod hst1_histogram_v1_generated;
 
 #[rustfmt::skip]
