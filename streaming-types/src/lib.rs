5f4f3bb3ae0152ba425141a2e076a5f91d828d76
Dan Nixon <dan@dan-nixon.com>
2023-09-01 14:09:56 +0100

    Add clippy::unnecessary_cast to flatbuffer ignore rules

--- streaming-types/src/lib.rs
+++ streaming-types/src/lib.rs
@@ -17,7 +17,7 @@ pub mod dev1_digitizer_event_v1_generated;
 pub mod hst1_histogram_v1_generated;
 
 #[rustfmt::skip]
-#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::extra_unused_lifetimes, clippy::missing_safety_doc, clippy::size_of_in_element_count)]
+#[allow(unused_imports, clippy::derivable_impls, clippy::derive_partial_eq_without_eq, clippy::extra_unused_lifetimes, clippy::missing_safety_doc, clippy::size_of_in_element_count, clippy::unnecessary_cast)]
 pub mod frame_metadata_v1_generated;
 
 pub use flatbuffers;
