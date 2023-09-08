cab019eef1e515cffa899e6aaaae082262af4685
Henry Denny <henry.denny12@gmail.com>
2023-09-08 11:01:41 +0100

    Kafka DAQ Report (#54)
    * Create kafka-daq-report directory.
    
    * Remove references to file and metric crates.
    
    * Create TUI frame for displaying important data.
    
    * Fix event processing.
    
    * Update UI.
    
    * Move Kafka message polling routine to tokio task.
    
    * Rename functions.
    
    * Fix bugs and warnings.
    
    * Update table headings.
    
    * Update table body generation and move to ui crate.
    
    * Increment message count for a particular digitiser id when message received.
    
    * Calculate channel data.
    
    * Calculate ast message frame and display.
    
    * Refactor code.
    
    * Update table ui and calculate remaining values.
    
    * Refactor code into app crate.
    
    * Sort table by digitiser ID.
    
    * Add comments.
    
    * Update README.md
    
    * Fix rustfmt formatting.
    
    * Fix Clippy.
    
    * Fix Clippy.
    
    * Fix Cargo.toml file.
    
    * Fix Clippy.
    
    * Remove title banner.
    
    * Fix invalid indexes.
    
    * Remove unnecessary option.
    
    * Update kafka-daq-report/src/main.rs
    
    Co-authored-by: Dan Nixon <dan@dan-nixon.com>
    
    * Remove unnecessary workspace resolver.
    
    * Update gitignore file.
    
    * Fix formatting.
    
    * Fix formatting.
    
    * Remove observability code.
    
    * Remove unnecessary option.
    
    ---------
    
    Fixes #44 
    
    Co-authored-by: Henry Denny <henry.denny@stfc.ac.uk>
    Co-authored-by: Dan Nixon <dan@dan-nixon.com>

---
+++ kafka-daq-report/src/app.rs
@@ -0,0 +1,132 @@
+use chrono::{DateTime, Timelike, Utc};
+use ratatui::widgets::TableState;
+
+use super::SharedData;
+
+/// Holds the current state of the program.
+pub struct App {
+    pub table_headers: Vec<String>,
+    pub table_body: Vec<Vec<String>>,
+    pub table_state: TableState,
+}
+
+impl App {
+    /// Create a new instance with default values.
+    pub fn new() -> App {
+        App {
+            table_headers: [
+                "Digitiser ID",          // 1
+                "#Msgs Received",        // 2
+                "First Msg Timestamp",   // 3
+                "Last Msg Timestamp",    // 4
+                "Last Msg Frame",        // 5
+                "#Present Channels",     // 6
+                "#Channels Changed?",    // 7
+                "First Channel Samples", // 8
+                "#Samples Identical?",   // 9
+                "#Samples Changed?",     // 10
+            ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+            table_body: Vec::new(),
+            table_state: TableState::default(),
+        }
+    }
+
+    pub fn generate_table_body(&mut self, shared_data: SharedData) {
+        // Clear table body.
+        self.table_body.clear();
+        let logged_data = shared_data.lock().unwrap();
+        // Sort by digitiser ID.
+        let mut sorted_data: Vec<_> = logged_data.iter().collect();
+        sorted_data.sort_by_key(|x| x.0);
+        // Add rows to table.
+        for (digitiser_id, digitiser_data) in sorted_data.iter() {
+            self.table_body.push(vec![
+                // 1. Digitiser ID.
+                digitiser_id.to_string(),
+                // 2. Number of messages received.
+                format!("{}", digitiser_data.num_msg_received),
+                // 3. First message timestamp.
+                format_timestamp(digitiser_data.first_msg_timestamp),
+                // 4. Last message timestamp.
+                format_timestamp(digitiser_data.last_msg_timestamp),
+                // 5. Last message frame.
+                format!("{}", digitiser_data.last_msg_frame),
+                // 6. Number of channels present.
+                format!("{}", digitiser_data.num_channels_present),
+                // 7. Has the number of channels changed?
+                format!(
+                    "{}",
+                    match digitiser_data.has_num_channels_changed {
+                        true => "Yes",
+                        false => "No",
+                    }
+                ),
+                // 8. Number of samples in the first channel.
+                format!("{}", digitiser_data.num_samples_in_first_channel),
+                // 9. Is the number of samples identical?
+                format!(
+                    "{}",
+                    match digitiser_data.is_num_samples_identical {
+                        true => "Yes",
+                        false => "No",
+                    }
+                ),
+                // 10. Has the number of samples changed?
+                format!(
+                    "{}",
+                    match digitiser_data.has_num_samples_changed {
+                        true => "Yes",
+                        false => "No",
+                    }
+                ),
+            ])
+        }
+    }
+
+    /// Move to the next item in the table.
+    pub fn next(&mut self) {
+        if self.table_body.is_empty() {
+            return;
+        }
+        let i = match self.table_state.selected() {
+            Some(i) => {
+                if i >= self.table_body.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.table_state.select(Some(i));
+    }
+
+    /// Move to the previous item in the table.
+    pub fn previous(&mut self) {
+        if self.table_body.is_empty() {
+            return;
+        }
+        let i = match self.table_state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.table_body.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.table_state.select(Some(i));
+    }
+}
+
+/// Create a neatly formatted String from a timestamp.
+fn format_timestamp(timestamp: Option<DateTime<Utc>>) -> String {
+    match timestamp {
+        None => "N/A".to_string(),
+        Some(t) => format!("{}\n{} ns", t.format("%H:%M:%S"), t.nanosecond(),),
+    }
+}
---
+++ kafka-daq-report/src/main.rs
@@ -0,0 +1,281 @@
+mod app;
+mod ui;
+
+use crate::app::App;
+use anyhow::Result;
+use chrono::{DateTime, Utc};
+use clap::Parser;
+use crossterm::event::{self, DisableMouseCapture, EnableMouseCapture, Event as CEvent, KeyCode};
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use ratatui::{prelude::CrosstermBackend, Terminal};
+use rdkafka::{
+    consumer::{stream_consumer::StreamConsumer, CommitMode, Consumer},
+    message::Message,
+};
+use std::collections::HashMap;
+use std::{
+    io,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+    time::{Duration, Instant},
+};
+use streaming_types::dat1_digitizer_analog_trace_v1_generated::{
+    digitizer_analog_trace_message_buffer_has_identifier, root_as_digitizer_analog_trace_message,
+};
+use tokio::task;
+use ui::ui;
+
+/// Holds required data for a specific digitiser.
+pub struct DigitiserData {
+    pub num_msg_received: usize,
+    pub first_msg_timestamp: Option<DateTime<Utc>>,
+    pub last_msg_timestamp: Option<DateTime<Utc>>,
+    pub last_msg_frame: u32,
+    pub num_channels_present: usize,
+    pub has_num_channels_changed: bool,
+    pub num_samples_in_first_channel: usize,
+    pub is_num_samples_identical: bool,
+    pub has_num_samples_changed: bool,
+}
+
+impl DigitiserData {
+    /// Create a new instance with default values.
+    pub fn new(
+        timestamp: Option<DateTime<Utc>>,
+        frame: u32,
+        num_channels_present: usize,
+        num_samples_in_first_channel: usize,
+        is_num_samples_identical: bool,
+    ) -> Self {
+        DigitiserData {
+            num_msg_received: 1,
+            first_msg_timestamp: timestamp,
+            last_msg_timestamp: timestamp,
+            last_msg_frame: frame,
+            num_channels_present,
+            has_num_channels_changed: false,
+            num_samples_in_first_channel,
+            is_num_samples_identical,
+            has_num_samples_changed: false,
+        }
+    }
+}
+
+type SharedData = Arc<Mutex<HashMap<u8, DigitiserData>>>;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(long)]
+    broker: String,
+
+    #[clap(long)]
+    username: Option<String>,
+
+    #[clap(long)]
+    password: Option<String>,
+
+    #[clap(long = "group")]
+    consumer_group: String,
+
+    #[clap(long)]
+    trace_topic: String,
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let args = Cli::parse();
+    log::debug!("Args: {:?}", args);
+
+    let consumer: StreamConsumer =
+        common::generate_kafka_client_config(&args.broker, &args.username, &args.password)
+            .set("group.id", &args.consumer_group)
+            .set("enable.partition.eof", "false")
+            .set("session.timeout.ms", "6000")
+            .set("enable.auto.commit", "false")
+            .create()?;
+
+    consumer.subscribe(&[&args.trace_topic])?;
+
+    // Set up terminal.
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // Set up app and shared data.
+    let mut app = App::new();
+
+    let shared_data: SharedData = Arc::new(Mutex::new(HashMap::new()));
+
+    // Set up event polling.
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+
+    // Event polling thread.
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate && tx.send(Event::Tick).is_ok() {
+                last_tick = Instant::now();
+            }
+        }
+    });
+
+    // Message polling thread.
+    task::spawn(poll_kafka_msg(consumer, Arc::clone(&shared_data)));
+
+    // Run app.
+    loop {
+        // Poll events.
+        match rx.recv()? {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => break,
+                KeyCode::Down => app.next(),
+                KeyCode::Up => app.previous(),
+                _ => (),
+            },
+            Event::Tick => (),
+        }
+
+        // Use the current data to regenerate the table body (may be inefficient to call every time).
+        app.generate_table_body(Arc::clone(&shared_data));
+
+        // Draw terminal using shared data.
+        terminal.draw(|frame| ui(frame, &mut app))?;
+    }
+
+    // Clean up terminal.
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    terminal.clear()?;
+
+    Ok(())
+}
+
+/// Poll kafka messages and update digitiser data.
+async fn poll_kafka_msg(consumer: StreamConsumer, shared_data: SharedData) {
+    loop {
+        match consumer.recv().await {
+            Err(e) => log::warn!("Kafka error: {}", e),
+            Ok(msg) => {
+                log::debug!(
+                    "key: '{:?}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+                    msg.key(),
+                    msg.topic(),
+                    msg.partition(),
+                    msg.offset(),
+                    msg.timestamp()
+                );
+
+                if let Some(payload) = msg.payload() {
+                    if digitizer_analog_trace_message_buffer_has_identifier(payload) {
+                        match root_as_digitizer_analog_trace_message(payload) {
+                            Ok(data) => {
+                                // Update digitiser data.
+                                let mut logged_data = shared_data.lock().unwrap();
+
+                                let frame_number = data.metadata().frame_number();
+
+                                let num_channels_present = match data.channels() {
+                                    Some(c) => c.len(),
+                                    None => 0,
+                                };
+                                let num_samples_in_first_channel = match data.channels() {
+                                    Some(c) => c.get(0).voltage().unwrap().len(),
+                                    None => 0,
+                                };
+                                let is_num_samples_identical = match data.channels() {
+                                    Some(c) => || -> bool {
+                                        for trace in c.iter() {
+                                            if trace.voltage().unwrap().len()
+                                                != num_samples_in_first_channel
+                                            {
+                                                return false;
+                                            }
+                                        }
+                                        true
+                                    }(),
+                                    None => false,
+                                };
+
+                                let timestamp =
+                                    data.metadata().timestamp().copied().map(|t| t.into());
+
+                                logged_data
+                                    .entry(data.digitizer_id())
+                                    .and_modify(|d| {
+                                        d.num_msg_received += 1;
+
+                                        d.last_msg_timestamp = timestamp;
+                                        let num_channels = match data.channels() {
+                                            Some(c) => c.len(),
+                                            None => 0,
+                                        };
+                                        if !d.has_num_channels_changed {
+                                            d.has_num_channels_changed =
+                                                num_channels != d.num_channels_present;
+                                        }
+                                        d.num_channels_present = num_channels;
+                                        if !d.has_num_channels_changed {
+                                            d.has_num_samples_changed = num_samples_in_first_channel
+                                                != d.num_samples_in_first_channel;
+                                        }
+                                        d.num_samples_in_first_channel =
+                                            num_samples_in_first_channel;
+                                        d.is_num_samples_identical = is_num_samples_identical;
+                                    })
+                                    .or_insert(DigitiserData::new(
+                                        timestamp,
+                                        frame_number,
+                                        num_channels_present,
+                                        num_samples_in_first_channel,
+                                        is_num_samples_identical,
+                                    ));
+
+                                log::info!(
+                                    "Trace packet: dig. ID: {}, metadata: {:?}",
+                                    data.digitizer_id(),
+                                    data.metadata()
+                                );
+                            }
+                            Err(e) => {
+                                log::warn!("Failed to parse message: {}", e);
+                            }
+                        }
+                    } else {
+                        log::warn!("Unexpected message type on topic \"{}\"", msg.topic());
+                    }
+                }
+
+                consumer.commit_message(&msg, CommitMode::Async).unwrap();
+            }
+        };
+    }
+}
