41a39d442af5d81b30d8e75c3e48a130aaba658a
Henry Denny <henry.denny12@gmail.com>
2023-09-19 13:40:21 +0100

    Remove redundant build inputs.

--- flake.nix
+++ flake.nix
@@ -32,42 +32,37 @@
         version = ws_cargo.workspace.package.version;
         git_revision = self.shortRev or self.dirtyShortRev;
 
-        nativeBuildInputs = with pkgs; [ cmake pkg-config flatbuffers ];
-        buildInputs = with pkgs; [ openssl ];
+        nativeBuildInputs = with pkgs; [ cmake flatbuffers ];
 
       in rec {
         devShell = pkgs.mkShell {
           nativeBuildInputs = nativeBuildInputs ++ [ toolchain.toolchain ];
-          buildInputs = buildInputs;
           packages = with pkgs; [ nix skopeo ];
         };
 
         packages = {
-          events-to-histogram = import ./events-to-histogram { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
-          kafka-daq-report = import ./kafka-daq-report { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
-          simulator = import ./simulator { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
-          stream-to-file = import ./stream-to-file { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
-          trace-archiver = import ./trace-archiver { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
-          trace-to-events = import ./trace-to-events { inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs; };
+          events-to-histogram = import ./events-to-histogram { inherit pkgs naersk' version git_revision nativeBuildInputs; };
+          kafka-daq-report = import ./kafka-daq-report { inherit pkgs naersk' version git_revision nativeBuildInputs; };
+          simulator = import ./simulator { inherit pkgs naersk' version git_revision nativeBuildInputs; };
+          stream-to-file = import ./stream-to-file { inherit pkgs naersk' version git_revision nativeBuildInputs; };
+          trace-archiver = import ./trace-archiver { inherit pkgs naersk' version git_revision nativeBuildInputs; };
+          trace-to-events = import ./trace-to-events { inherit pkgs naersk' version git_revision nativeBuildInputs; };
 
           fmt = naersk'.buildPackage {
             src = ./.;
             nativeBuildInputs = nativeBuildInputs;
-            buildInputs = buildInputs;
             mode = "fmt";
           };
 
           clippy = naersk'.buildPackage {
             src = ./.;
             nativeBuildInputs = nativeBuildInputs;
-            buildInputs = buildInputs;
             mode = "clippy";
           };
 
           test = naersk'.buildPackage {
             src = ./.;
             nativeBuildInputs = nativeBuildInputs;
-            buildInputs = buildInputs;
             mode = "test";
             # Ensure detailed test output appears in nix build log
             cargoTestOptions = x: x ++ ["1>&2"];
