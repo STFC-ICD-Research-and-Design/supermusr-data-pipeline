2a98fd87647174bc811ca00da4e72fd65183f3d2
Daniel Kirk <daniel.kirk@stfc.ac.uk>
2023-10-12 14:50:36 +0000

    Commit

--- overlays/tdengine/default.nix
+++ overlays/tdengine/default.nix
@@ -4,28 +4,37 @@
   fetchFromGitHub
 }: stdenv.mkDerivation {
   name = "TDengine-client";
-  version = "3.0.4.2";
+  version = "3.1.1.7";
 
   src = fetchFromGitHub {
     owner = "taosdata";
     repo = "TDEngine";
-    rev = "ver-3.0.4.2";
-    hash = "sha256-CMpfaVhq3LOngugxp9POvXIQMjtpgwqP1VoCj2KkfYE=";
+    rev = "ver-3.1.1.0";
+    hash = "sha256-Aua3i7YIo4U56KaOZuLJEAc5fIxG5Pux9LM2TATwl40=";
   };
-
-  dontUseCmakeConfigure=true;
 
   nativeBuildInputs = with nixpkgs; [
     cacert
     git
     pkg-config
     xz
+    libuv
     jansson
     cmake
+    apr
+    aprutil
+    curl
+    geos
   ];
 
+  #buildPhase = ''
+  #  bash ./build.sh
+  #'';
   buildPhase = ''
-    ./build.sh
+    mkdir debug
+    cd debug
+    cmake .. -DBUILD_TOOLS=false
+    make
   '';
   
   installPhase = ''
