5a96326b58f4764551749cc955bc29b24faff78b
Dan Nixon <dan@dan-nixon.com>
2023-12-06 14:45:16 +0000

    Do not init env_logger in kafka-daq-report
    Doing so ruins TUI formatting.

--- kafka-daq-report/src/main.rs
+++ kafka-daq-report/src/main.rs
@@ -92,8 +92,6 @@ enum Event<I> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
-
     let args = Cli::parse();
     log::debug!("Args: {:?}", args);
 
