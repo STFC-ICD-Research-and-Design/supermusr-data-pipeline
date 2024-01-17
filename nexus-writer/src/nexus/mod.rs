b6d317c660f54bea158b8809e4f686993ea42b51
Modularius <daniel.kirk@rocketmail.com>
2024-01-17 12:06:45 +0000

    Update

--- nexus-writer/src/nexus/mod.rs
+++ nexus-writer/src/nexus/mod.rs
@@ -1,41 +1,57 @@
-
-use std::{path::{PathBuf, Path}, fmt::Display};
-
-use hdf5::{file::File, H5Type, Extents, Group, SimpleExtents};
+use std::fmt::Display;
 use anyhow::{anyhow, Result};
+use hdf5::{
+    types::VarLenUnicode,
+    H5Type,
+    Group,
+    Location
+};
 
 mod builder;
 pub(crate) use builder::Nexus;
 
-pub(crate) fn add_classed_group_to_group(parent : &Group, name: &str, class: &str) -> Result<Group> {
-    let mut group = parent.create_group(name)?;
-    add_nx_class_to_group(&group, class);
+type AttributeList<'a> = &'a [(&'static str,&'static str)];
+
+
+fn add_attribute_to(parent : &Location, attr: &str, value: &str) -> Result<()> {
+    parent.new_attr_builder().with_data(&[value.parse::<VarLenUnicode>()?]).create(attr)?;
+    Ok(())
+}
+
+pub(crate) fn add_attribute_list_to(field : &Location, attrs: AttributeList) -> Result<()> {
+    for (attr,value) in attrs {
+        add_attribute_to(field, attr,value)?;
+    }
+    Ok(())
+}
+
+fn set_group_nx_class(parent : &Group, class: &str) -> Result<()> {
+    add_attribute_to(parent, "NX_class", class)
+}
+
+pub(crate) fn add_new_group_to(parent : &Group, name: &str, class: &str) -> Result<Group> {
+    let group = parent.create_group(name)?;
+    set_group_nx_class(&group, class)?;
     Ok(group)
 }
 
-pub(crate) fn add_field_to_group<T : H5Type + Display + Copy> (parent : &Group, name: &str, content: T) -> Result<()> {
+pub(crate) fn add_new_field_to<T : H5Type + Display + Copy>(parent : &Group, name: &str, content: T, attrs : AttributeList) -> Result<()> {
     match parent.new_dataset_builder().with_data(&[content]).create(name) {
-        Ok(_) => Ok(()),
+        Ok(field) => add_attribute_list_to(&field, attrs),
         Err(e) => Err(anyhow!("Could not add field: {name}={content} to {0}. Error: {e}",parent.name()))
     }
 }
-pub(crate) fn add_string_field_to_group (parent : &Group, name: &str, content: &str) -> Result<()> {
-    match parent.new_dataset_builder().with_data(&[content.parse::<hdf5::types::VarLenUnicode>()?]).create(name) {
-        Ok(data) => Ok(()),
+
+pub(crate) fn add_new_string_field_to(parent : &Group, name: &str, content: &str, attrs : AttributeList) -> Result<()> {
+    match parent.new_dataset_builder().with_data(&[content.parse::<VarLenUnicode>()?]).create(name) {
+        Ok(field) => add_attribute_list_to(&field, attrs),
         Err(e) => Err(anyhow!("Could not add string field: {name}={content} to {0}. Error: {e}",parent.name()))
     }
 }
-pub(crate) fn add_slice_field_to_group<T : H5Type> (parent : &Group, name: &str, content: &[T]) -> Result<()> {
+
+pub(crate) fn add_new_slice_field_to<T : H5Type>(parent : &Group, name: &str, content: &[T], attrs : AttributeList) -> Result<()> {
     match parent.new_dataset_builder().with_data(content).create(name) {
-        Ok(_) => Ok(()),
+        Ok(field) => add_attribute_list_to(&field, attrs),
         Err(e) => Err(anyhow!("Could not add slice: {name}=[...] to {0}. Error: {e}",parent.name()))
     }
-}
-fn add_nx_class_to_group(parent : &Group, class: &str) -> Result<()> {
-    add_attribute_to_group(parent, "NX_class", class)
-}
-
-fn add_attribute_to_group(parent : &Group, attr: &str, value: &str) -> Result<()> {
-    parent.new_attr_builder().with_data(value).create(attr)?;
-    Ok(())
 }
