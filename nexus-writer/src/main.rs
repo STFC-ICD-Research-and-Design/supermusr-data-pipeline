8a36e919b85836204a0c9828de578752f4a97734
Modularius <daniel.kirk@rocketmail.com>
2024-01-23 14:32:18 +0000

    Added RunStart/Stop Schemas

--- nexus-writer/src/main.rs
+++ nexus-writer/src/main.rs
@@ -4,7 +4,7 @@ mod nexus;
 use anyhow::{anyhow, Result};
 use chrono as _;
 use clap::Parser;
-use kagiyama::{AlwaysReady, Watcher};
+//use kagiyama::{AlwaysReady, Watcher};
 use ndarray as _;
 use ndarray_stats as _;
 use nexus::{EventList, Nexus};
@@ -14,7 +14,11 @@ use rdkafka::{
     message::Message,
 };
 use std::{net::SocketAddr, path::PathBuf};
-use supermusr_streaming_types::dev1_digitizer_event_v1_generated::root_as_digitizer_event_list_message;
+use supermusr_streaming_types::{
+    dev1_digitizer_event_v1_generated::root_as_digitizer_event_list_message,
+    ecs_pl72_run_start_generated::{root_as_run_start, run_start_buffer_has_identifier},
+    ecs_6s4t_run_stop_generated::{root_as_run_stop, run_stop_buffer_has_identifier},
+};
 
 //  To run trace-reader
 // cargo run --bin trace-reader -- --broker localhost:19092 --consumer-group trace-producer --trace-topic Traces --file-name ../Data/Traces/MuSR_A41_B42_C43_D44_Apr2021_Ag_ZF_IntDeg_Slit60_short.traces --number-of-trace-events 20 --random-sample
@@ -23,7 +27,7 @@ use supermusr_streaming_types::dev1_digitizer_event_v1_generated::root_as_digiti
 // cargo run --bin trace-to-events -- --broker localhost:19092 --trace-topic Traces --event-topic Events --group trace-to-events constant-phase-discriminator --threshold-trigger=-40,1,0
 
 // To run nexus-writer
-// cargo run --bin nexus-writer -- --broker localhost:19092 --consumer-group nexus-writer --event-topic Events --file-name output
+// cargo run --bin nexus-writer -- --broker localhost:19092 --consumer-group nexus-writer --control_topic controls --event-topic Events --file-name output/Saves
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 struct Cli {
@@ -40,10 +44,10 @@ struct Cli {
     consumer_group: String,
 
     #[clap(long)]
-    event_topic: Option<String>,
+    control_topic: String,
 
     #[clap(long)]
-    trace_topic: Option<String>,
+    event_topic: Option<String>,
 
     #[clap(long)]
     histogram_topic: Option<String>,
@@ -65,9 +69,9 @@ async fn main() -> Result<()> {
     let args = Cli::parse();
     log::debug!("Args: {:?}", args);
 
-    let mut watcher = Watcher::<AlwaysReady>::default();
-    metrics::register(&mut watcher);
-    watcher.start_server(args.observability_address).await;
+//    let mut watcher = Watcher::<AlwaysReady>::default();
+//    metrics::register(&mut watcher);
+//    watcher.start_server(args.observability_address).await;
 
     let consumer: StreamConsumer = supermusr_common::generate_kafka_client_config(
         &args.broker,
@@ -80,7 +84,8 @@ async fn main() -> Result<()> {
     .set("enable.auto.commit", "false")
     .create()?;
 
-    let topics_to_subscribe = [args.event_topic.as_deref(), args.histogram_topic.as_deref()]
+    //  This line can be simplified when is it clear which topics we need
+    let topics_to_subscribe = [Some(args.control_topic.as_str()), args.event_topic.as_deref(), args.histogram_topic.as_deref()]
         .into_iter()
         .flatten()
         .collect::<Vec<&str>>();
@@ -91,6 +96,7 @@ async fn main() -> Result<()> {
     }
     consumer.subscribe(&topics_to_subscribe)?;
 
+    let mut state = false;
     let mut nexus = Nexus::<EventList>::new();
 
     let mut count = 0; //  Will be removed in future version
@@ -118,13 +124,13 @@ async fn main() -> Result<()> {
                         .event_topic
                         .as_deref()
                         .map(|topic| msg.topic() == topic)
-                        .unwrap_or(false)
+                        .unwrap_or(false)   //  This statement should certainly be simplified
                     {
                         match root_as_digitizer_event_list_message(payload) {
                             Ok(data) => {
                                 metrics::MESSAGES_RECEIVED
                                     .get_or_create(&metrics::MessagesReceivedLabels::new(
-                                        metrics::MessageKind::Trace,
+                                        metrics::MessageKind::Event,
                                     ))
                                     .inc();
                                 if let Err(e) = nexus.process_message(&data) {
@@ -143,6 +149,45 @@ async fn main() -> Result<()> {
                                         metrics::FailureKind::UnableToDecodeMessage,
                                     ))
                                     .inc();
+                            }
+                        }
+                    } else if args.control_topic == msg.topic() {
+                        if run_start_buffer_has_identifier(payload) {
+                            match root_as_run_start(payload) {
+                                Ok(data) => {
+                                    metrics::MESSAGES_RECEIVED
+                                        .get_or_create(&metrics::MessagesReceivedLabels::new(
+                                            metrics::MessageKind::Unknown,
+                                        ))
+                                        .inc();
+                                    
+                                },
+                                Err(e) => {
+                                    log::warn!("Failed to parse message: {}", e);
+                                    metrics::FAILURES
+                                        .get_or_create(&metrics::FailureLabels::new(
+                                            metrics::FailureKind::UnableToDecodeMessage,
+                                        ))
+                                        .inc();
+                                },
+                            }
+                        } else if run_stop_buffer_has_identifier(payload) {
+                            match root_as_run_stop(payload) {
+                                Ok(data) => {
+                                    metrics::MESSAGES_RECEIVED
+                                        .get_or_create(&metrics::MessagesReceivedLabels::new(
+                                            metrics::MessageKind::Unknown,
+                                        ))
+                                        .inc();
+                                },
+                                Err(e) => {
+                                    log::warn!("Failed to parse message: {}", e);
+                                    metrics::FAILURES
+                                        .get_or_create(&metrics::FailureLabels::new(
+                                            metrics::FailureKind::UnableToDecodeMessage,
+                                        ))
+                                        .inc();
+                                },
                             }
                         }
                     } else {
