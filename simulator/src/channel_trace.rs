2c2e1225727bd6117582db5876af8cc724ad9087
Modularius <daniel.kirk@rocketmail.com>
2024-01-30 11:13:27 +0000

    Begun Advanced Simulation Mode

---
+++ simulator/src/channel_trace.rs
@@ -0,0 +1,59 @@
+use supermusr_common::{Intensity, Time};
+
+pub(crate) enum Pulse {
+    Flat {
+        start: Time,
+        stop: Time,
+        amplitude: Intensity,
+    },
+    Triangular {
+        start: Time,
+        peak: Time,
+        stop: Time,
+        amplitude: Intensity,
+    },
+    Gaussian {
+        mean : Time,
+        sd: f64,
+        peak_amplitude: Intensity,
+    },
+    Biexp {
+
+    }
+}
+
+impl Pulse {
+    pub(crate) fn value(&self, time: Time) -> f64 {
+        match self {
+            &Self::Flat {start, stop, amplitude} =>
+                if start <= time && time < stop {
+                    amplitude as f64
+                } else {
+                    f64::default()
+                },
+            &Self::Triangular {start, peak, stop, amplitude} =>
+                if start <= time && time < peak {
+                    amplitude as f64*(peak - time) as f64/(peak - start) as f64
+                } else if peak <= time && time < stop {
+                    amplitude as f64*(time - peak) as f64/(stop - peak) as f64
+                } else {
+                    f64::default()
+                },
+            &Self::Gaussian { mean, sd, peak_amplitude } => {
+                peak_amplitude as f64*f64::exp(-f64::powi(0.5*(time as f64 - mean as f64)/sd,2))
+            },
+            &Self::Biexp {} => f64::default(),
+        }
+    }
+}
+
+pub(crate) fn generate_trace(samples : Time, pulses: Vec<Pulse>, noise: Vec<f64>) -> Vec<Intensity> {
+    (0..samples).map(|time|
+        pulses.iter().map(|p|p.value(time)).sum::<f64>()
+        + noise.iter().enumerate().map(|(i,n)|
+            f64::default()
+        ).sum::<f64>()
+    )
+    .map(|x : f64|x as Intensity)
+    .collect()
+}
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,5 @@
+mod channel_trace;
+
 use chrono::Utc;
 use clap::{Parser, Subcommand};
 use rdkafka::{
@@ -19,6 +21,9 @@ use supermusr_streaming_types::{
     frame_metadata_v1_generated::{FrameMetadataV1, FrameMetadataV1Args, GpsTime},
 };
 use tokio::time;
+use channel_trace::generate_trace;
+
+use crate::channel_trace::Pulse;
 
 #[derive(Clone, Parser)]
 #[clap(author, version, about)]
@@ -73,6 +78,9 @@ struct Single {
     /// Number of frame to be sent
     #[clap(long = "frame", default_value = "0")]
     frame_number: u32,
+
+    #[command(subcommand)]
+    trace_mode: TraceMode,
 }
 
 #[derive(Clone, Parser)]
@@ -84,6 +92,32 @@ struct Continuous {
     /// Time in milliseconds between each frame
     #[clap(long, default_value = "20")]
     frame_time: u64,
+
+    #[command(subcommand)]
+    trace_mode: TraceMode,
+}
+
+
+
+#[derive(Clone, Subcommand)]
+enum TraceMode {
+    Basic,
+    Advanced(AdvancedTrace),
+}
+
+#[derive(Clone, Parser)]
+struct AdvancedTrace {
+    /// Number of pulses to generate
+    #[clap(long = "num_pulses", default_value = "100")]
+    num_pulses: usize,
+
+    /// Number of channels to generate
+    #[clap(long = "num_channels", default_value = "8")]
+    num_channels: Channel,
+
+    /// Amount of noise to include
+    #[clap(long = "noise", default_value = "0")]
+    noise: usize,
 }
 
 #[tokio::main]
@@ -108,6 +142,7 @@ async fn main() {
                 cli.clone(),
                 &mut fbb,
                 m.frame_number,
+                &m.trace_mode,
                 Duration::default(),
             )
             .await;
@@ -120,7 +155,7 @@ async fn main() {
 
             loop {
                 let now = SystemTime::now().duration_since(start_time).unwrap();
-                send(&producer, cli.clone(), &mut fbb, frame_number, now).await;
+                send(&producer, cli.clone(), &mut fbb, frame_number, &m.trace_mode, now).await;
 
                 frame_number += 1;
                 frame.tick().await;
@@ -134,6 +169,7 @@ async fn send(
     cli: Cli,
     fbb: &mut FlatBufferBuilder<'_>,
     frame_number: u32,
+    trace_mode: &TraceMode,
     now: Duration,
 ) {
     let time: GpsTime = Utc::now().into();
@@ -198,24 +234,55 @@ async fn send(
         };
         let metadata = FrameMetadataV1::create(fbb, &metadata);
 
-        let mut channel0_voltage = Vec::<Intensity>::new();
-        channel0_voltage.resize(cli.measurements_per_frame, 404);
-        channel0_voltage[0] = frame_number as Intensity;
-        channel0_voltage[1] = cli.digitizer_id as Intensity;
-        let channel0_voltage = Some(fbb.create_vector::<Intensity>(&channel0_voltage));
-        let channel0 = ChannelTrace::create(
-            fbb,
-            &ChannelTraceArgs {
-                channel: 0,
-                voltage: channel0_voltage,
-            },
-        );
+        let message = match trace_mode {
+            TraceMode::Basic => {
+                let mut channel0_voltage = Vec::<Intensity>::new();
+                channel0_voltage.resize(cli.measurements_per_frame, 404);
+                channel0_voltage[0] = frame_number as Intensity;
+                channel0_voltage[1] = cli.digitizer_id as Intensity;
+                let channel0_voltage = Some(fbb.create_vector::<Intensity>(&channel0_voltage));
+                let channel0 = ChannelTrace::create(
+                    fbb,
+                    &ChannelTraceArgs {
+                        channel: 0,
+                        voltage: channel0_voltage,
+                    },
+                );
 
-        let message = DigitizerAnalogTraceMessageArgs {
-            digitizer_id: cli.digitizer_id,
-            metadata: Some(metadata),
-            sample_rate: 1_000_000_000,
-            channels: Some(fbb.create_vector(&[channel0])),
+                DigitizerAnalogTraceMessageArgs {
+                    digitizer_id: cli.digitizer_id,
+                    metadata: Some(metadata),
+                    sample_rate: 1_000_000_000,
+                    channels: Some(fbb.create_vector(&[channel0])),
+                }
+            },
+            TraceMode::Advanced(advanced_trace) => {
+                let channels = (0..advanced_trace.num_channels).map(|i| {
+                    let channel_voltage = generate_trace(
+                        cli.measurements_per_frame as Time,
+                        (0..advanced_trace.num_pulses).map(|_|
+                            Pulse::Gaussian { mean: 0, sd: 1.0, peak_amplitude: 40 }
+                        ).collect(),
+                        vec![]
+                    );
+                    let channel_voltage = Some(fbb.create_vector::<Intensity>(&channel_voltage));
+                    ChannelTrace::create(
+                        fbb,
+                        &ChannelTraceArgs {
+                            channel: i,
+                            voltage: channel_voltage,
+                        },
+                    )
+                })
+                .collect::<Vec<_>>();
+
+                DigitizerAnalogTraceMessageArgs {
+                    digitizer_id: cli.digitizer_id,
+                    metadata: Some(metadata),
+                    sample_rate: 1_000_000_000,
+                    channels: Some(fbb.create_vector(&channels)),
+                }
+            }
         };
         let message = DigitizerAnalogTraceMessage::create(fbb, &message);
         finish_digitizer_analog_trace_message_buffer(fbb, message);
