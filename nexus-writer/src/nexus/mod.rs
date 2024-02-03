a9d1a8b327cc000f061c7c25d3b705137484f348
Modularius <daniel.kirk@rocketmail.com>
2024-02-03 21:38:21 +0000

    Update mod.rs
    Added nexus class string constants

--- nexus-writer/src/nexus/mod.rs
+++ nexus-writer/src/nexus/mod.rs
@@ -6,3 +6,13 @@ mod writer;
 pub(crate) use builder::Nexus;
 pub(crate) use messages::EventList;
 use runs::{Run, RunParameters};
+
+mod NexusClass {
+  const root = "NX_root";
+  const entry = "NXentry";
+  const instrument = "NXinstrument";
+  const period = "NXperiod";
+  const entry = "NXsource";
+  const detector = "NXdetector";
+  const event_data = "NXevent_data";
+}
