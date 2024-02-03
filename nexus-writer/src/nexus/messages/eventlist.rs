5cc75975da0f88cc9a8d293b4227e33d709deebd
Modularius <daniel.kirk@rocketmail.com>
2024-02-03 22:12:54 +0000

    Update eventlist.rs
    Standardized timestamp format and reworked new field slice.

--- nexus-writer/src/nexus/messages/eventlist.rs
+++ nexus-writer/src/nexus/messages/eventlist.rs
@@ -5,6 +5,8 @@ use hdf5::Group;
 use supermusr_common::{Channel, Time};
 use supermusr_streaming_types::dev1_digitizer_event_v1_generated::DigitizerEventListMessage;
 
+const TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S%.f%z";
+
 #[derive(Default, Debug, Clone)]
 pub(crate) struct EventMessage {
     number_of_events: usize,
@@ -130,23 +132,24 @@ impl ListType for EventList {
         //add_new_slice_field_to::<u32>(detector, "spectrum_index", &[0], &[])?;
         //add_new_slice_field_to::<u32>(detector, "data", &[], &[])?;
 
-        add_new_slice_field_to(detector, "pulse_height", &self.pulse_height, &[])?;
-        add_new_slice_field_to(detector, "event_id", &self.event_id, &[])?;
-        add_new_slice_field_to(detector, "event_time_offset", &self.event_time_offset, &[])?;
-        add_new_slice_field_to(
-            detector,
-            "event_time_zero",
-            &self.event_time_zero,
+        add_new_slice_field_to(detector, "pulse_height", &self.pulse_height)?;
+        add_new_slice_field_to(detector, "event_id", &self.event_id)?;
+        add_new_slice_field_to(detector, "event_time_offset", &self.event_time_offset)?;
+        add_new_slice_field_to(detector, "event_index", &self.event_index)?;
+
+        // Note to self, please update writer.rs. Attributes are so infrequently added it is better
+        // to remove them from the new field functions, and return the field instead. 
+        let event_time_zero = add_new_slice_field_to(detector, "event_time_zero", &self.event_time_zero)?;
+        set_attribute_list_to(&event_time_zero,
             &[(
                 "offset",
                 &self
                     .offset
                     .ok_or(anyhow!("Offset not set: {0:?}", self))?
-                    .format("%Y-%m-%dT%H:%M:%S%.f%z")
+                    .format(TIMESTAMP_FORMAT)
                     .to_string(),
-            )],
+            )]
         )?;
-        add_new_slice_field_to(detector, "event_index", &self.event_index, &[])?;
         Ok(())
     }
 }
