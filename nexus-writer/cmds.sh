a0ecbc42ca469dfa824b8a88cab47285bf1a4380
Modularius <daniel.kirk@rocketmail.com>
2024-02-09 11:02:29 +0000

    Fixed Test Script

--- nexus-writer/cmds.sh
+++ nexus-writer/cmds.sh
@@ -1,5 +1,3 @@
-cargo run --bin trace-reader -- --broker localhost:19092 --consumer-group trace-producer --trace-topic Traces --file-name ../Data/Traces/MuSR_A41_B42_C43_D44_Apr2021_Ag_ZF_IntDeg_Slit60_short.traces --number-of-trace-events 20 --random-sample
-
 cargo run --bin trace-to-events -- \
     --broker localhost:19092 --group trace-to-events \
     --trace-topic Traces --event-topic Events \
@@ -14,3 +12,9 @@ cargo run --bin run-simulator -- --broker localhost:19092 --topic Controls --run
 cargo run --bin trace-reader -- --broker localhost:19092 --consumer-group trace-producer --trace-topic Traces --file-name ../Data/Traces/MuSR_A41_B42_C43_D44_Apr2021_Ag_ZF_IntDeg_Slit60_short.traces --number-of-trace-events 20 --random-sample
 cargo run --bin run-simulator -- --broker localhost:19092 --topic Controls --run-name Test run-stop
 
+echo "Waiting five seconds for trace-to-events and nexus-writer to complete"
+sleep 5
+
+pkill trace-to-event
+pkill nexus-writer
+echo "Script complete"
