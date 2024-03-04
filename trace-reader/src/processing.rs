238d9dfaaca68b38dd2fc892295c1cab70ae0e72
Modularius <daniel.kirk@rocketmail.com>
2024-03-04 10:10:46 +0000

    Fixed Clippy Warning

--- trace-reader/src/processing.rs
+++ trace-reader/src/processing.rs
@@ -3,8 +3,7 @@
 
 use super::loader::{TraceFile, TraceFileEvent};
 use anyhow::{Error, Result};
-use chrono::{DateTime, Utc};
-use log::{debug, error};
+use chrono::{DateTime,Utc};
 use rdkafka::{
     producer::{FutureProducer, FutureRecord},
     util::Timeout,
