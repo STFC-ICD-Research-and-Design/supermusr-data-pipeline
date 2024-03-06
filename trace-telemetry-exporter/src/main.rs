b45793e2ce1dfe82672abd94c68d63b0c259f221
Henry Denny <henry.denny12@gmail.com>
2024-03-06 15:07:45 +0000

    Fix bug with the recent message count hashmap.

--- trace-telemetry-exporter/src/main.rs
+++ trace-telemetry-exporter/src/main.rs
@@ -135,7 +135,7 @@ async fn update_message_rate(recent_msg_counts: MessageCounts, message_rate_inte
     loop {
         // Wait a set period of time before calculating average.
         sleep(Duration::from_secs(message_rate_interval)).await;
-        
+
         let mut recent_msg_counts = recent_msg_counts.lock().unwrap();
         // Calculate and record message rate for each digitiser.
         recent_msg_counts
@@ -145,7 +145,7 @@ async fn update_message_rate(recent_msg_counts: MessageCounts, message_rate_inte
                 let labels = [get_digitiser_label(*digitiser_id)];
                 gauge!("digitiser_message_received_rate", &labels).set(msg_rate);
             });
-            
+
         // Reset recent message count for each digitiser.
         recent_msg_counts.clear();
     }
@@ -160,7 +160,8 @@ fn process_message(data: &DigitizerAnalogTraceMessage<'_>, recent_msg_counts: Me
         .lock()
         .unwrap()
         .entry(id)
-        .and_modify(|d| *d += 1);
+        .and_modify(|d| *d += 1)
+        .or_insert(1);
 
     /* Metrics */
     // Message recieved count
