642af0070381211ae79df014967629ef0d775a7d
Henry Denny <henry.denny12@gmail.com>
2024-04-03 15:51:35 +0100

    Fix formatting.

--- trace-to-events/src/main.rs
+++ trace-to-events/src/main.rs
@@ -4,20 +4,20 @@ mod processing;
 mod pulse_detection;
 
 use clap::Parser;
-use kagiyama::{AlwaysReady, Watcher};
 use parameters::{DetectorSettings, Mode, Polarity};
 use rdkafka::{
     consumer::{stream_consumer::StreamConsumer, CommitMode, Consumer},
     message::Message,
     producer::{FutureProducer, FutureRecord},
 };
-use std::{net::SocketAddr, path::PathBuf, time::Duration};
-use supermusr_common::{metrics::{
-    failures::{FAILURE_KIND_KAFKA_PUBLISH_FAILED, FAILURE_KIND_UNABLE_TO_DECODE_MESSAGE},
-    messages_received::{MESSAGE_KIND_TRACE, MESSAGE_KIND_UNKNOWN},
-}, Intensity};
-use supermusr_streaming_types::dat1_digitizer_analog_trace_v1_generated::{
-    digitizer_analog_trace_message_buffer_has_identifier, root_as_digitizer_analog_trace_message,
+use std::{net::SocketAddr, path::PathBuf};
+use supermusr_common::{
+    metrics::{
+        failures::{FAILURE_KIND_KAFKA_PUBLISH_FAILED, FAILURE_KIND_UNABLE_TO_DECODE_MESSAGE},
+        messages_received::{MESSAGE_KIND_TRACE, MESSAGE_KIND_UNKNOWN},
+    },
+    Intensity,
+};
 use supermusr_streaming_types::{
     dat1_digitizer_analog_trace_v1_generated::{
         digitizer_analog_trace_message_buffer_has_identifier,
