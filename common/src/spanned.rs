9774872b00846a86a2d88dad21aff8e0e9bd298f
Modularius <daniel.kirk@rocketmail.com>
2024-04-26 10:23:43 +0000

    Added error handling to the SpanOnce type and documentation

--- common/src/spanned.rs
+++ common/src/spanned.rs
@@ -1,6 +1,29 @@
-use std::{fmt::Debug, ops::{Deref, DerefMut}};
+use std::{
+    fmt::Debug,
+    ops::{Deref, DerefMut},
+};
+use thiserror::Error;
 use tracing::Span;
 
+#[derive(Error, Debug)]
+pub enum SpanOnceError {
+    #[error("Attempt to initialise twice")]
+    AlreadyInit,
+    #[error("Attempt to initialise a spent span")]
+    SpentInit,
+    #[error("Attempt to read an uninitialised span")]
+    UninitialisedRead,
+    #[error("Attempt to read a spent span")]
+    SpentRead,
+    #[error("Attempt to inherit from an uninitialised span")]
+    UninitialisedInherit,
+    #[error("Attempt to inherit from a spent span")]
+    SpentInherit,
+}
+
+/// A wrapper for use by types implementing the Spanned and SpannedMut trait.
+/// This type can only be set once, read immutably, and inherited by a new
+/// uninitialised SpanOnce.
 #[derive(Default)]
 pub enum SpanOnce {
     #[default]
@@ -10,91 +33,64 @@ pub enum SpanOnce {
 }
 
 impl SpanOnce {
-    pub fn init(&mut self, span: Span) {
+    pub fn init(&mut self, span: Span) -> Result<(), SpanOnceError> {
         *self = match self {
             SpanOnce::Waiting => SpanOnce::Spanned(span),
-            _ => panic!(),
-        }
+            SpanOnce::Spanned(_) => return Err(SpanOnceError::AlreadyInit),
+            SpanOnce::Spent => return Err(SpanOnceError::SpentInit),
+        };
+        Ok(())
     }
-    pub fn get(&self) -> &Span {
+    pub fn get(&self) -> Result<&Span, SpanOnceError> {
         match &self {
-            SpanOnce::Spanned(span) => span,
-            _ => panic!(),
+            SpanOnce::Spanned(span) => Ok(span),
+            SpanOnce::Waiting => Err(SpanOnceError::UninitialisedRead),
+            SpanOnce::Spent => Err(SpanOnceError::SpentRead),
         }
     }
-    pub fn inherit(&mut self) -> SpanOnce {
+    pub fn inherit(&mut self) -> Result<SpanOnce, SpanOnceError> {
         let span = match &self {
             SpanOnce::Spanned(span) => span.clone(),
-            _ => panic!(),
+            SpanOnce::Waiting => return Err(SpanOnceError::UninitialisedInherit),
+            SpanOnce::Spent => return Err(SpanOnceError::SpentInherit),
         };
         *self = SpanOnce::Spent;
-        SpanOnce::Spanned(span)
+        Ok(SpanOnce::Spanned(span))
     }
 }
+
+/// Types which have a span: SpanOnce field implement one or both of
+/// Spanned or SpannedMut. Their purpose is to return references
+/// to the SpanOnce field. For instance:
+/// ```rust
+/// # struct SpanOnce;
+/// # struct Foo { span: SpanOnce }
+/// # pub trait Spanned { fn span(&self) -> &SpanOnce; }
+/// # pub trait SpannedMut: Spanned { fn span_mut(&mut self) -> &mut SpanOnce; }
+/// 
+/// impl Spanned for Foo {
+///     fn span(&self) -> &SpanOnce {
+///         &self.span
+///     }
+/// }
+/// 
+/// impl SpannedMut for Foo {
+///     fn span_mut(&mut self) -> &mut SpanOnce {
+///         &mut self.span
+///     }
+/// }
+/// ```
 pub trait Spanned {
     fn span(&self) -> &SpanOnce;
 }
-pub trait SpannedMut : Spanned {
+pub trait SpannedMut: Spanned {
     fn span_mut(&mut self) -> &mut SpanOnce;
 }
 
-/// This is a wrapper for a type which can be bundled with a span.
-/// Given type Foo, define trait FooLike in the following fashion:
-/// ```rust
-/// # #[derive(Debug)] struct Foo;
-/// # impl AsMut<Foo> for Foo { fn as_mut(&mut self) -> &mut Foo { self } }
-/// # impl AsRef<Foo> for Foo { fn as_ref(&self) -> &Foo { self } }
-/// trait FooLike : std::fmt::Debug + AsRef<Foo> + AsMut<Foo> {
-///     fn new(/* ... */) -> Self where Self: Sized;
-/// }
-/// // and implement for both Foo and Spanned<Foo>, that is:
-/// impl FooLike for Foo {
-///     fn new(/* ... */) -> Foo {
-///         # unreachable!()
-///         /* ... */
-///     }
-/// }
-/// // and
-/// # use supermusr_common::tracer::Spanned;
-/// impl FooLike for Spanned<Foo> {
-///     fn new(/* ... */) -> Spanned<Foo> {
-///         # unreachable!()
-///         /* ... */
-///     }
-/// }
-/// ```
-/// Now any function or struct that uses Foo, can use a generic that implements FooType instead.
-/// For instance
-/// ```rust
-/// # struct Foo; impl Foo { fn some_foo(&self) {} }
-/// struct Bar {
-///     foo : Foo,
-/// }
-/// impl Bar {
-///     fn do_some_foo(&self) {
-///         self.foo.some_foo()
-///     }
-/// }
-/// ```
-/// becomes:
-/// ```rust
-/// # #[derive(Debug)] struct Foo; impl Foo { fn some_foo(&self) {} }
-/// # impl AsMut<Foo> for Foo { fn as_mut(&mut self) -> &mut Foo { self } }
-/// # impl AsRef<Foo> for Foo { fn as_ref(&self) -> &Foo { self } }
-/// trait FooLike : std::fmt::Debug + AsRef<Foo> + AsMut<Foo> {
-///     fn new(/* ... */) -> Self where Self: Sized;
-/// }
-/// struct Bar<F : FooLike> {
-///     foo : F,
-/// }
-/// impl<F : FooLike> Bar<F> {
-///     fn do_some_foo(&self) {
-///         self.foo.as_ref().some_foo()
-///     }
-/// }
-/// ```
-/// So now Foo and Spanned<Foo> can be switched out easily,
-/// by using either `Bar<Foo>` or `Bar<Spanned<Foo>>`.
+/// This generic type wraps a type and associates a SpanOnce with it.
+/// This is an alternative to implementing Spanned or SpannedMut for the type.
+/// Note this does not currently implement SpannedMut but could be easily
+/// extended in the future.
 pub struct SpanWrapper<T> {
     span: SpanOnce,
     value: T,
@@ -117,7 +113,10 @@ impl<T: Default> SpanWrapper<T> {
 
 impl<T> SpanWrapper<T> {
     pub fn new(span: Span, value: T) -> Self {
-        Self { span: SpanOnce::Spanned(span), value }
+        Self {
+            span: SpanOnce::Spanned(span),
+            value,
+        }
     }
 
     pub fn new_with_current(value: T) -> Self {
