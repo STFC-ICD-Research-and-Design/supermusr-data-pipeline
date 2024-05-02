198877fa9a76abd4401d68f2d6fb55e144364cfb
Henry Denny <henry.denny12@gmail.com>
2024-05-02 12:38:47 +0100

    Remove handling of events from `stream-to-file`. (#165)
    * Remove event handling.
    
    * Rename `stream-to-file` to `trace-archiver-hdf5`.
    
    * Update `Cargo.lock`.
    
    * Replace `stream-to-file` with `trace-archiver-hdf5`.
    
    * Revert changes to `july_test.md`.
    
    * Make trace topic and file be required.
    
    * Make digitizer count be required.
    
    * Update trace-archiver-hdf5/src/main.rs
    
    Co-authored-by: Dan Nixon <dan@dan-nixon.com>
    
    * Update trace-archiver-hdf5/src/main.rs
    
    Co-authored-by: Dan Nixon <dan@dan-nixon.com>
    
    * Make rename consistent.
    
    * Revert `topic` back to `trace_topic`.
    
    This is so that there is less ambiguity when the `control_topic` command
    line argument is added later.
    
    ---------
    
    Co-authored-by: Dan Nixon <dan@dan-nixon.com>

--- flake.nix
+++ flake.nix
@@ -91,8 +91,8 @@
           // import ./nexus-writer {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs hdf5-joined;}
           // import ./run-simulator {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs;}
           // import ./simulator {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs;}
-          // import ./stream-to-file {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs hdf5-joined;}
           // import ./trace-archiver {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs hdf5-joined;}
+          // import ./trace-archiver-hdf5 {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs hdf5-joined;}
           // import ./trace-archiver-tdengine {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs;}
           // import ./trace-reader {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs;}
           // import ./trace-telemetry-exporter {inherit pkgs naersk' version git_revision nativeBuildInputs buildInputs;}
--- trace-archiver-hdf5/src/main.rs
+++ trace-archiver-hdf5/src/main.rs
@@ -1,8 +1,8 @@
 mod file;
 mod metrics;
 
-use crate::file::{EventFile, TraceFile};
-use anyhow::{anyhow, Result};
+use crate::file::TraceFile;
+use anyhow::Result;
 use clap::Parser;
 use kagiyama::{prometheus::metrics::info::Info, AlwaysReady, Watcher};
 use rdkafka::{
@@ -10,15 +10,8 @@ use rdkafka::{
     message::Message,
 };
 use std::{net::SocketAddr, path::PathBuf};
-use supermusr_streaming_types::{
-    aev2_frame_assembled_event_v2_generated::{
-        frame_assembled_event_list_message_buffer_has_identifier,
-        root_as_frame_assembled_event_list_message,
-    },
-    dat2_digitizer_analog_trace_v2_generated::{
-        digitizer_analog_trace_message_buffer_has_identifier,
-        root_as_digitizer_analog_trace_message,
-    },
+use supermusr_streaming_types::dat2_digitizer_analog_trace_v2_generated::{
+    digitizer_analog_trace_message_buffer_has_identifier, root_as_digitizer_analog_trace_message,
 };
 use tracing::{debug, info, warn};
 
@@ -38,19 +31,13 @@ struct Cli {
     consumer_group: String,
 
     #[clap(long)]
-    event_topic: Option<String>,
+    trace_topic: String,
 
     #[clap(long)]
-    event_file: Option<PathBuf>,
+    file: PathBuf,
 
     #[clap(long)]
-    trace_topic: Option<String>,
-
-    #[clap(long)]
-    trace_file: Option<PathBuf>,
-
-    #[clap(long)]
-    digitizer_count: Option<usize>,
+    digitizer_count: usize,
 
     #[clap(long, env, default_value = "127.0.0.1:9090")]
     observability_address: SocketAddr,
@@ -66,22 +53,7 @@ async fn main() -> Result<()> {
     let mut watcher = Watcher::<AlwaysReady>::default();
     metrics::register(&mut watcher);
     {
-        let output_files = Info::new(vec![
-            (
-                "event".to_string(),
-                match args.event_file {
-                    Some(ref f) => f.display().to_string(),
-                    None => "none".into(),
-                },
-            ),
-            (
-                "trace".to_string(),
-                match args.trace_file {
-                    Some(ref f) => f.display().to_string(),
-                    None => "none".into(),
-                },
-            ),
-        ]);
+        let output_files = Info::new(vec![("trace".to_string(), args.file.display().to_string())]);
 
         let mut registry = watcher.metrics_registry();
         registry.register("output_files", "Configured output filenames", output_files);
@@ -99,31 +71,9 @@ async fn main() -> Result<()> {
     .set("enable.auto.commit", "false")
     .create()?;
 
-    let topics_to_subscribe: Vec<String> = vec![args.event_topic, args.trace_topic]
-        .into_iter()
-        .flatten()
-        .collect();
-    let topics_to_subscribe: Vec<&str> = topics_to_subscribe.iter().map(|i| i.as_ref()).collect();
-    if topics_to_subscribe.is_empty() {
-        return Err(anyhow!(
-            "Nothing to do (no message type requested to be saved)"
-        ));
-    }
-    consumer.subscribe(&topics_to_subscribe)?;
+    consumer.subscribe(&[&args.trace_topic])?;
 
-    let mut event_file = match args.event_file {
-        Some(filename) => Some(EventFile::create(&filename)?),
-        None => None,
-    };
-
-    let mut trace_file = match args.trace_file {
-        Some(filename) => Some(TraceFile::create(
-            &filename,
-            args.digitizer_count
-                .expect("digitizer count should be provided"),
-        )?),
-        None => None,
-    };
+    let mut trace_file = TraceFile::create(&args.file, args.digitizer_count)?;
 
     loop {
         match consumer.recv().await {
@@ -139,39 +89,8 @@ async fn main() -> Result<()> {
                 );
 
                 if let Some(payload) = msg.payload() {
-                    if event_file.is_some()
-                        && frame_assembled_event_list_message_buffer_has_identifier(payload)
-                    {
-                        match root_as_frame_assembled_event_list_message(payload) {
-                            Ok(data) => {
-                                info!("Event packet: metadata: {:?}", data.metadata());
-                                metrics::MESSAGES_RECEIVED
-                                    .get_or_create(&metrics::MessagesReceivedLabels::new(
-                                        metrics::MessageKind::Event,
-                                    ))
-                                    .inc();
-                                if let Err(e) = event_file.as_mut().unwrap().push(&data) {
-                                    warn!("Failed to save events to file: {}", e);
-                                    metrics::FAILURES
-                                        .get_or_create(&metrics::FailureLabels::new(
-                                            metrics::FailureKind::FileWriteFailed,
-                                        ))
-                                        .inc();
-                                }
-                            }
-                            Err(e) => {
-                                warn!("Failed to parse message: {}", e);
-                                metrics::FAILURES
-                                    .get_or_create(&metrics::FailureLabels::new(
-                                        metrics::FailureKind::UnableToDecodeMessage,
-                                    ))
-                                    .inc();
-                            }
-                        }
-                        consumer.commit_message(&msg, CommitMode::Async).unwrap();
-                    } else if trace_file.is_some()
-                        && digitizer_analog_trace_message_buffer_has_identifier(payload)
-                    {
+                    if digitizer_analog_trace_message_buffer_has_identifier(payload) {
+                        // In other words, if the message is from the trace topic.
                         match root_as_digitizer_analog_trace_message(payload) {
                             Ok(data) => {
                                 info!(
@@ -184,7 +103,7 @@ async fn main() -> Result<()> {
                                         metrics::MessageKind::Trace,
                                     ))
                                     .inc();
-                                if let Err(e) = trace_file.as_mut().unwrap().push(&data) {
+                                if let Err(e) = trace_file.push(&data) {
                                     warn!("Failed to save traces to file: {}", e);
                                     metrics::FAILURES
                                         .get_or_create(&metrics::FailureLabels::new(
