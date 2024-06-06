a26c410c2e05e63e3d43851e326557c489ae00b8
Modularius <daniel.kirk@rocketmail.com>
2024-06-06 22:16:56 +0000

    Added propagator.rs

---
+++ common/src/tracer/propagator.rs
@@ -0,0 +1,111 @@
+use opentelemetry::propagation::{Extractor, Injector};
+use rdkafka::{
+    message::{BorrowedHeaders, Headers, OwnedHeaders},
+    producer::FutureRecord,
+};
+use tracing::{debug, Span};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+
+pub(super) struct HeaderInjector<'a>(pub &'a mut OwnedHeaders);
+
+impl<'a> Injector for HeaderInjector<'a> {
+    fn set(&mut self, key: &str, value: String) {
+        let mut new = OwnedHeaders::new().insert(rdkafka::message::Header {
+            key,
+            value: Some(&value),
+        });
+
+        for header in self.0.iter() {
+            let s = String::from_utf8(header.value.unwrap().to_vec()).unwrap();
+            new = new.insert(rdkafka::message::Header {
+                key: header.key,
+                value: Some(&s),
+            });
+        }
+
+        self.0.clone_from(&new);
+    }
+}
+
+struct HeaderExtractor<'a>(pub &'a BorrowedHeaders);
+
+impl<'a> Extractor for HeaderExtractor<'a> {
+    fn get(&self, key: &str) -> Option<&str> {
+        for i in 0..self.0.count() {
+            if let Ok(val) = self.0.get_as::<str>(i) {
+                if val.key == key {
+                    return val.value;
+                }
+            }
+        }
+        None
+    }
+
+    fn keys(&self) -> Vec<&str> {
+        self.0.iter().map(|kv| kv.key).collect::<Vec<_>>()
+    }
+}
+
+
+
+/// May be used when the component produces messages.
+/// The `conditional_` prefix indicates a bool should be passed,
+/// indicating whether OpenTelemetry is used.
+/// If this is false, the methods usually do nothing.
+pub trait FutureRecordTracerExt {
+    fn optional_headers(self, headers: Option<OwnedHeaders>) -> Self;
+    fn conditional_inject_current_span_into_headers(self, use_otel: bool) -> Self;
+    fn conditional_inject_span_into_headers(self, use_otel: bool, span: &Span) -> Self;
+}
+
+impl FutureRecordTracerExt for FutureRecord<'_, str, [u8]> {
+    fn optional_headers(self, headers: Option<OwnedHeaders>) -> Self {
+        if let Some(headers) = headers {
+            self.headers(headers)
+        } else {
+            self
+        }
+    }
+
+    fn conditional_inject_current_span_into_headers(self, use_otel: bool) -> Self {
+        self.conditional_inject_span_into_headers(use_otel, &tracing::Span::current())
+    }
+
+    fn conditional_inject_span_into_headers(self, use_otel: bool, span: &Span) -> Self {
+        if use_otel {
+            let mut headers = self.headers.clone().unwrap_or_default();
+            opentelemetry::global::get_text_map_propagator(|propagator| {
+                propagator.inject_context(&span.context(), &mut HeaderInjector(&mut headers))
+            });
+            self.headers(headers)
+        } else {
+            self
+        }
+    }
+}
+
+/// May be used when the component consumne messages.
+/// The `conditional_` prefix indicates a bool should be passed,
+/// indicating whether OpenTelemetry is used.
+/// If this is false, the methods usually do nothing.
+pub trait OptionalHeaderTracerExt {
+    fn conditional_extract_to_current_span(self, use_otel: bool);
+    fn conditional_extract_to_span(self, use_otel: bool, span: &Span);
+}
+
+impl OptionalHeaderTracerExt for Option<&BorrowedHeaders> {
+    fn conditional_extract_to_current_span(self, use_otel: bool) {
+        self.conditional_extract_to_span(use_otel, &tracing::Span::current())
+    }
+
+    fn conditional_extract_to_span(self, use_otel: bool, span: &Span) {
+        if let Some(headers) = self {
+            if use_otel {
+                debug!("Kafka Header Found");
+                span.set_parent(opentelemetry::global::get_text_map_propagator(
+                    |propagator| propagator.extract(&HeaderExtractor(headers)),
+                ));
+            }
+        }
+    }
+}
