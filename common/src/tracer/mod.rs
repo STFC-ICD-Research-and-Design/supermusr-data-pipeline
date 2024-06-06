26a15525beeb87b2a64ab0866c9882f47ee83d5e
Modularius <daniel.kirk@rocketmail.com>
2024-06-06 22:16:24 +0000

    Broke tracer.rs into multiple files

---
+++ common/src/tracer/mod.rs
@@ -0,0 +1,26 @@
+mod otel_tracer;
+mod tracer_engine;
+mod propagator;
+
+pub use otel_tracer::{OtelTracer, OtelOptions};
+pub use propagator::{FutureRecordTracerExt, OptionalHeaderTracerExt};
+pub use tracer_engine::{TracerEngine, TracerOptions};
+
+/// Should be called at the start of each component
+/// The `conditional_` prefix used in the methods of FutureRecordTracerExt and OptionalHeaderTracerExt
+/// indicate the method's first parameter is a bool, however here the first parameter is an Option<&str>
+/// with the URL of the OpenTelemetry collector to be used, or None, if OpenTelemetry is not used.
+#[macro_export]
+macro_rules! conditional_init_tracer {
+    ($options:expr) => {{
+        let tracer = TracerEngine::new($options, env!("CARGO_BIN_NAME"), module_path!());
+        // This is called here (in the macro) rather than as part of `TracerEngine::new`
+        // to ensure the warning is emitted in the correct module.
+        if tracer.is_some() {
+            if let Err(e) = tracer.set_otel_error_handler(|e| warn!("{e}")) {
+                warn!("{e}");
+            }
+        }
+        tracer
+    }};
+}
---
+++ common/src/tracer/tracer_engine.rs
@@ -0,0 +1,77 @@
+use opentelemetry::global::Error;
+use tracing::Span;
+use tracing_opentelemetry::OpenTelemetrySpanExt;
+use tracing_subscriber::{
+    layer::SubscriberExt,
+    EnvFilter, Layer,
+};
+
+use super::otel_tracer::{OtelTracer, OtelOptions};
+
+pub struct TracerOptions<'a> {
+    pub otel_options: Option<OtelOptions<'a>>,
+}
+
+/// This object initialises all tracers, given a TracerOptions struct.
+/// If TracerOptions contains a OtelOptions struct then it initialises the
+/// OtelTracer object as well.
+pub struct TracerEngine {
+    use_otel: bool,
+}
+
+impl TracerEngine {
+    pub fn is_some(&self) -> bool {
+        self.use_otel
+    }
+    pub fn set_otel_error_handler<F>(&self, f: F) -> Result<(), Error>
+    where
+        F: Fn(Error) + Send + Sync + 'static,
+    {
+        opentelemetry::global::set_error_handler(f)
+    }
+}
+
+impl TracerEngine {
+    /// Initialises the stdout tracer, and (if required) the OpenTelemetry service for the crate
+    /// #Arguments
+    /// * `options` - The caller-specified instance of TracerOptions.
+    /// * `service_name` - The name of the OpenTelemetry service to assign to the crate.
+    /// * `module_name` - The name of the current module.
+    /// #Returns
+    /// An instance of TracerEngine
+    pub fn new(options: TracerOptions, service_name: &str, module_name: &str) -> Self {
+        let use_otel = options.otel_options.is_some();
+
+        let stdout_tracer = tracing_subscriber::fmt::layer().with_writer(std::io::stdout);
+
+        let otel_tracer = options.otel_options.and_then(|otel_options| {
+            OtelTracer::<_>::new(otel_options, service_name, module_name).ok()
+        });
+        // If otel_tracer did not work, update the use_otel variable
+        let use_otel = use_otel && otel_tracer.is_some();
+
+        // This filter is applied to the stdout tracer
+        let log_filter = EnvFilter::from_default_env();
+
+        let subscriber = tracing_subscriber::Registry::default()
+            .with(stdout_tracer.with_filter(log_filter))
+            .with(otel_tracer.map(|otel_tracer| otel_tracer.layer));
+
+        //  This is only called once, so will never panic
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("tracing::subscriber::set_global_default should only be called once");
+
+        Self { use_otel }
+    }
+
+    /// Sets a span's parent to other_span
+    pub fn set_span_parent_to(span: &Span, parent_span: &Span) {
+        span.set_parent(parent_span.context());
+    }
+}
+
+impl Drop for TracerEngine {
+    fn drop(&mut self) {
+        opentelemetry::global::shutdown_tracer_provider()
+    }
+}
