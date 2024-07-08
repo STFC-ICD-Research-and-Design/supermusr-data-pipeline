388b91ff94f783d02cdd62cfe1c4e7c2f23cda98
Henry Denny <henry.denny12@gmail.com>
2024-07-08 12:22:27 +0100

    Sort metrics alphabetically for better readability.

--- common/src/metrics.rs
+++ common/src/metrics.rs
@@ -1,19 +1,19 @@
 pub mod metric_names {
-    pub const MESSAGES_PROCESSED: &str = "messages_processed";
     pub const FAILURES: &str = "failures";
+    pub const MESSAGES_PROCESSED: &str = "messages_processed";
     pub const MESSAGES_RECEIVED: &str = "messages_received";
 }
 
 pub mod messages_received {
     #[derive(Debug, Clone, Eq, Hash, PartialEq)]
     pub enum MessageKind {
-        Trace,
+        Alarm,
         Event,
         LogData,
-        SampleEnvironmentData,
-        Alarm,
         RunStart,
         RunStop,
+        SampleEnvironmentData,
+        Trace,
         Unexpected,
     }
 
@@ -22,13 +22,13 @@ pub mod messages_received {
         (
             "message_kind",
             match message_kind {
-                MessageKind::Trace => "trace",
+                MessageKind::Alarm => "alarm",
                 MessageKind::Event => "event",
                 MessageKind::LogData => "log_data",
-                MessageKind::SampleEnvironmentData => "sample_environment_data",
-                MessageKind::Alarm => "alarm",
                 MessageKind::RunStart => "run_start",
                 MessageKind::RunStop => "run_stop",
+                MessageKind::SampleEnvironmentData => "sample_environment_data",
+                MessageKind::Trace => "trace",
                 MessageKind::Unexpected => "unexpected",
             },
         )
@@ -38,11 +38,11 @@ pub mod messages_received {
 pub mod failures {
     #[derive(Debug, Clone, Eq, Hash, PartialEq)]
     pub enum FailureKind {
-        UnableToDecodeMessage,
         DataProcessingFailed,
-        KafkaPublishFailed,
         FileWriteFailed,
         InvalidMetadata,
+        KafkaPublishFailed,
+        UnableToDecodeMessage,
     }
 
     // Label building function
@@ -50,11 +50,11 @@ pub mod failures {
         (
             "failure_kind",
             match failure_kind {
-                FailureKind::UnableToDecodeMessage => "unable_to_decode_message",
                 FailureKind::DataProcessingFailed => "data_processing_failed",
-                FailureKind::KafkaPublishFailed => "kafka_publish_failed",
                 FailureKind::FileWriteFailed => "file_write_failed",
                 FailureKind::InvalidMetadata => "invalid_metadata",
+                FailureKind::KafkaPublishFailed => "kafka_publish_failed",
+                FailureKind::UnableToDecodeMessage => "unable_to_decode_message",
             },
         )
     }
