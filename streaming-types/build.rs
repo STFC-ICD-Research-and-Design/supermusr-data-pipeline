8e8773d6a5a9aa106aa9f79dbfca60d1c4f17272
Dan Nixon <dan@dan-nixon.com>
2024-08-09 16:03:51 +0100

    Improve flatbuffers code generation
    - Improve general readability of build.rs and remove some code
      duplication
    
    - Move generated code from `src` to `OUT_DIR` and adjust
      `streaming-types` library as appropriate
    
    - Unconditionally regenerate code on `cargo build` (this had caused a
      couple of issues in the past, and is a sufficiently cheap operation
      that it could just be done every time)

--- streaming-types/build.rs
+++ streaming-types/build.rs
@@ -1,30 +1,32 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let schema_dir = Path::new("../schemas/");
-    let target_dir = Path::new("./src/generated");
 
-    println!("cargo:rerun-if-changed={}", schema_dir.display());
+    let target_dir: PathBuf = std::env::var_os("OUT_DIR")
+        .expect("OUT_DIR should be set")
+        .into();
+    let target_dir = target_dir.join("flatbuffer_generated");
+
+    let inputs = [
+        "aev2_frame_assembled_event_v2.fbs",
+        "dat2_digitizer_analog_trace_v2.fbs",
+        "dev2_digitizer_event_v2.fbs",
+        "frame_metadata_v2.fbs",
+        "ecs_6s4t_run_stop.fbs",
+        "ecs_df12_det_spec_map.fbs",
+        "ecs_pl72_run_start.fbs",
+        "ecs_f144_logdata.fbs",
+        "ecs_se00_data.fbs",
+        "ecs_al00_alarm.fbs",
+    ];
+    let inputs: Vec<PathBuf> = inputs.iter().map(|i| schema_dir.join(i)).collect();
+    let inputs: Vec<&Path> = inputs.iter().map(|i| i.as_path()).collect();
 
     flatc_rust::run(flatc_rust::Args {
-        inputs: &[
-            schema_dir
-                .join("aev2_frame_assembled_event_v2.fbs")
-                .as_path(),
-            schema_dir
-                .join("dat2_digitizer_analog_trace_v2.fbs")
-                .as_path(),
-            schema_dir.join("dev2_digitizer_event_v2.fbs").as_path(),
-            schema_dir.join("frame_metadata_v2.fbs").as_path(),
-            schema_dir.join("ecs_6s4t_run_stop.fbs").as_path(),
-            schema_dir.join("ecs_df12_det_spec_map.fbs").as_path(),
-            schema_dir.join("ecs_pl72_run_start.fbs").as_path(),
-            schema_dir.join("ecs_f144_logdata.fbs").as_path(),
-            schema_dir.join("ecs_se00_data.fbs").as_path(),
-            schema_dir.join("ecs_al00_alarm.fbs").as_path(),
-        ],
-        out_dir: target_dir,
+        inputs: &inputs,
+        out_dir: &target_dir,
         ..Default::default()
     })
-    .expect("flatc");
+    .expect("flatbuffer schemas should be compiled");
 }
