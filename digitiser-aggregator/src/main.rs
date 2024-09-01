1a5719ca49d4e7c99ff118e3283fccce783c7228
Modularius <daniel.kirk@rocketmail.com>
2024-09-01 14:44:11 +0000

    Formatting

--- digitiser-aggregator/src/main.rs
+++ digitiser-aggregator/src/main.rs
@@ -24,10 +24,8 @@ use supermusr_common::{
     tracer::{FutureRecordTracerExt, OptionalHeaderTracerExt, TracerEngine, TracerOptions},
     CommonKafkaOpts, DigitizerId,
 };
-use supermusr_streaming_types::{
-    dev2_digitizer_event_v2_generated::{
-        digitizer_event_list_message_buffer_has_identifier, root_as_digitizer_event_list_message,
-    },
+use supermusr_streaming_types::dev2_digitizer_event_v2_generated::{
+    digitizer_event_list_message_buffer_has_identifier, root_as_digitizer_event_list_message,
 };
 use tokio::task::JoinSet;
 use tracing::{debug, error, info_span, level_filters::LevelFilter, warn};
