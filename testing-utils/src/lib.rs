c15618045abe8b438e7892a60f140d5ca28032ee
Dan Nixon <dan@dan-nixon.com>
2024-09-24 13:39:42 +0100

    fixup! Add testing utility crate

--- testing-utils/src/lib.rs
+++ testing-utils/src/lib.rs
@@ -2,8 +2,4 @@ mod cargo;
 mod network;
 mod podman;
 
-pub use self::{
-    cargo::CargoBinaryRunner,
-    network::wait_for_url,
-    podman::PodmanDriver,
-};
+pub use self::{cargo::CargoBinaryRunner, network::wait_for_url, podman::PodmanDriver};
