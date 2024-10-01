ec3f585e033b2d667948f3451cc4df9cd3ecda56
Modularius <daniel.kirk@rocketmail.com>
2024-10-01 09:44:44 +0000

    Refactored into data.rs

--- diagnostics/src/daq_trace/app.rs
+++ diagnostics/src/daq_trace/app.rs
@@ -1,4 +1,4 @@
-use super::DigitiserDataHashMap;
+use super::{data::DigitiserData, DigitiserDataHashMap};
 use chrono::{DateTime, Timelike, Utc};
 use ratatui::widgets::TableState;
 
@@ -13,23 +13,7 @@ impl App {
     /// Create a new instance with default values.
     pub fn new() -> App {
         App {
-            table_headers: [
-                "Digitiser ID",          // 1
-                "#Msgs Received",        // 2
-                "First Msg Timestamp",   // 3
-                "Last Msg Timestamp",    // 4
-                "Last Msg Frame",        // 5
-                "Message Rate (Hz)",     // 6
-                "#Present Channels",     // 7
-                "#Channels Changed?",    // 8
-                "First Channel Samples", // 9
-                "#Samples Identical?",   // 10
-                "#Samples Changed?",     // 11
-                "#Bad Frames?",          // 12
-            ]
-            .iter()
-            .map(|s| s.to_string())
-            .collect(),
+            table_headers: DigitiserData::generate_headers(),
             table_body: Vec::new(),
             table_state: TableState::default(),
         }
@@ -46,50 +30,7 @@ impl App {
         sorted_data.sort_by_key(|x| x.0);
         // Add rows to table.
         for (digitiser_id, digitiser_data) in sorted_data.iter() {
-            self.table_body.push(vec![
-                // 1. Digitiser ID.
-                digitiser_id.to_string(),
-                // 2. Number of messages received.
-                format!("{}", digitiser_data.msg_count),
-                // 3. First message timestamp.
-                format_timestamp(digitiser_data.first_msg_timestamp),
-                // 4. Last message timestamp.
-                format_timestamp(digitiser_data.last_msg_timestamp),
-                // 5. Last message frame.
-                format!("{}", digitiser_data.last_msg_frame),
-                // 6. Message rate.
-                format!("{:.1}", digitiser_data.msg_rate),
-                // 7. Number of channels present.
-                format!("{}", digitiser_data.num_channels_present),
-                // 8. Has the number of channels changed?
-                format!(
-                    "{}",
-                    match digitiser_data.has_num_channels_changed {
-                        true => "Yes",
-                        false => "No",
-                    }
-                ),
-                // 9. Number of samples in the first channel.
-                format!("{}", digitiser_data.num_samples_in_first_channel),
-                // 10. Is the number of samples identical?
-                format!(
-                    "{}",
-                    match digitiser_data.is_num_samples_identical {
-                        true => "Yes",
-                        false => "No",
-                    }
-                ),
-                // 11. Has the number of samples changed?
-                format!(
-                    "{}",
-                    match digitiser_data.has_num_samples_changed {
-                        true => "Yes",
-                        false => "No",
-                    }
-                ),
-                // 12. Number of Bad Frames
-                format!("{}", digitiser_data.bad_frame_count),
-            ])
+            self.table_body.push(digitiser_data.generate_row(**digitiser_id))
         }
     }
 
@@ -131,7 +72,7 @@ impl App {
 }
 
 /// Create a neatly formatted String from a timestamp.
-fn format_timestamp(timestamp: Option<DateTime<Utc>>) -> String {
+pub(crate) fn format_timestamp(timestamp: Option<DateTime<Utc>>) -> String {
     match timestamp {
         None => "N/A".to_string(),
         Some(t) => format!(
