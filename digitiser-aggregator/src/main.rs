064646ef92bfda9a853b88e2733c404092d7876e
Modularius <daniel.kirk@rocketmail.com>
2024-11-08 15:49:08 +0000

    Fix excessive memory usage in aggregator (#269)
    ## Summary of changes
    
    - Replaces `JoinSet` method of producing `frame_event_list` messages to
    Kakfa, with a dedicated long-running thread which is tasked with
    emitting frames to Kafka.
    - Every call to `cache_poll` drains the cache of completed frames from
    the front, until an incomplete frame is reached, and sends them to the
    dedicated thread.
    - Frames are emitted in the order they were begun (i.e. when its first
    digitiser event message arrived).
    
    ## Instruction for review/testing
    
    General code review. This change been tested and verified with simulated
    data, and on HiFi.
    
    Closes
    https://github.com/STFC-ICD-Research-and-Design/supermusr-data-pipeline/issues/265.

--- digitiser-aggregator/src/main.rs
+++ digitiser-aggregator/src/main.rs
@@ -3,7 +3,7 @@ mod frame;
 
 use crate::data::EventData;
 use clap::Parser;
-use frame::FrameCache;
+use frame::{AggregatedFrame, FrameCache};
 use metrics::counter;
 use metrics_exporter_prometheus::PrometheusBuilder;
 use rdkafka::{
@@ -32,8 +32,10 @@ use supermusr_streaming_types::{
     },
     flatbuffers::InvalidFlatbuffer,
 };
-use tokio::task::JoinSet;
-use tracing::{debug, error, info_span, instrument, level_filters::LevelFilter, warn, Instrument};
+use tokio::sync::mpsc::{error::TrySendError, Receiver, Sender};
+use tracing::{debug, error, info_span, instrument, level_filters::LevelFilter, warn};
+
+const PRODUCER_TIMEOUT: Timeout = Timeout::After(Duration::from_millis(100));
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -68,6 +70,11 @@ struct Cli {
     #[clap(long, default_value = "500")]
     cache_poll_ms: u64,
 
+    /// Size of the send frame buffer.
+    /// If this limit is exceeded, the component will exit.
+    #[clap(long, default_value = "1024")]
+    send_frame_buffer_size: usize,
+
     /// Endpoint on which Prometheus text format metrics are available
     #[clap(long, env, default_value = "127.0.0.1:9090")]
     observability_address: SocketAddr,
@@ -80,6 +87,9 @@ struct Cli {
     #[clap(long, default_value = "info")]
     otel_level: LevelFilter,
 }
+
+type AggregatedFrameToBufferSender = Sender<AggregatedFrame<EventData>>;
+type TrySendAggregatedFrameError = TrySendError<AggregatedFrame<EventData>>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -100,7 +110,7 @@ async fn main() -> anyhow::Result<()> {
         &[args.input_topic.as_str()],
     );
 
-    let producer = supermusr_common::generate_kafka_client_config(
+    let producer: FutureProducer = supermusr_common::generate_kafka_client_config(
         &kafka_opts.broker,
         &kafka_opts.username,
         &kafka_opts.password,
@@ -139,15 +149,22 @@ async fn main() -> anyhow::Result<()> {
         "Number of complete frames sent by the aggregator"
     );
 
-    let mut kafka_producer_thread_set = JoinSet::new();
-
     let mut cache_poll_interval = tokio::time::interval(Duration::from_millis(args.cache_poll_ms));
+
+    // Creates Send-Frame thread and returns channel sender
+    let channel_send = create_producer_task(
+        tracer.use_otel(),
+        args.send_frame_buffer_size,
+        &producer,
+        &args.output_topic,
+    );
+
     loop {
         tokio::select! {
             event = consumer.recv() => {
                 match event {
                     Ok(msg) => {
-                        process_kafka_message(tracer.use_otel(), &mut kafka_producer_thread_set, &mut cache, &producer, &args.output_topic, &msg).await;
+                        process_kafka_message(tracer.use_otel(), &channel_send, &mut cache, &msg).await?;
                         consumer.commit_message(&msg, CommitMode::Async)
                             .expect("Message should commit");
                     }
@@ -155,7 +172,7 @@ async fn main() -> anyhow::Result<()> {
                 };
             }
             _ = cache_poll_interval.tick() => {
-                cache_poll(tracer.use_otel(), &mut kafka_producer_thread_set, &mut cache, &producer, &args.output_topic).await;
+                cache_poll(&channel_send, &mut cache).await?;
             }
         }
     }
@@ -172,12 +189,10 @@ fn spanned_root_as_digitizer_event_list_message(
 #[instrument(skip_all, fields(kafka_message_timestamp_ms = msg.timestamp().to_millis()))]
 async fn process_kafka_message(
     use_otel: bool,
-    kafka_producer_thread_set: &mut JoinSet<()>,
+    channel_send: &AggregatedFrameToBufferSender,
     cache: &mut FrameCache<EventData>,
-    producer: &FutureProducer,
-    output_topic: &str,
     msg: &BorrowedMessage<'_>,
-) {
+) -> Result<(), TrySendAggregatedFrameError> {
     msg.headers().conditional_extract_to_current_span(use_otel);
 
     if let Some(payload) = msg.payload() {
@@ -189,15 +204,7 @@ async fn process_kafka_message(
             .increment(1);
             match spanned_root_as_digitizer_event_list_message(payload) {
                 Ok(msg) => {
-                    process_digitiser_event_list_message(
-                        use_otel,
-                        kafka_producer_thread_set,
-                        cache,
-                        producer,
-                        output_topic,
-                        msg,
-                    )
-                    .await;
+                    process_digitiser_event_list_message(channel_send, cache, msg).await?;
                 }
                 Err(e) => {
                     warn!("Failed to parse message: {}", e);
@@ -219,6 +226,7 @@ async fn process_kafka_message(
             .increment(1);
         }
     }
+    Ok(())
 }
 
 #[tracing::instrument(skip_all, fields(
@@ -232,13 +240,10 @@ async fn process_kafka_message(
     num_cached_frames = cache.get_num_partial_frames(),
 ))]
 async fn process_digitiser_event_list_message(
-    use_otel: bool,
-    kafka_producer_thread_set: &mut JoinSet<()>,
+    channel_send: &AggregatedFrameToBufferSender,
     cache: &mut FrameCache<EventData>,
-    producer: &FutureProducer,
-    output_topic: &str,
     msg: DigitizerEventListMessage<'_>,
-) {
+) -> Result<(), TrySendAggregatedFrameError> {
     match msg.metadata().try_into() {
         Ok(metadata) => {
             debug!("Event packet: metadata: {:?}", msg.metadata());
@@ -248,14 +253,7 @@ async fn process_digitiser_event_list_message(
 
             record_metadata_fields_to_span!(&metadata, tracing::Span::current());
 
-            cache_poll(
-                use_otel,
-                kafka_producer_thread_set,
-                cache,
-                producer,
-                output_topic,
-            )
-            .await;
+            cache_poll(channel_send, cache).await?;
         }
         Err(e) => {
             warn!("Invalid Metadata: {e}");
@@ -266,51 +264,103 @@ async fn process_digitiser_event_list_message(
             .increment(1);
         }
     }
+    Ok(())
 }
 
 #[tracing::instrument(skip_all, level = "trace")]
 async fn cache_poll(
-    use_otel: bool,
-    kafka_producer_thread_set: &mut JoinSet<()>,
+    channel_send: &AggregatedFrameToBufferSender,
     cache: &mut FrameCache<EventData>,
+) -> Result<(), TrySendAggregatedFrameError> {
+    let span = info_span!(target: "otel", "Frame Complete");
+    while let Some(frame) = cache.poll() {
+        let _guard = span.enter();
+
+        // For each frame that is ready to send,
+        // `try_send` appends it to the channel queue
+        //  without blocking
+        if let Err(e) = channel_send.try_send(frame) {
+            // If the queue is full (or another error occurs),
+            // then we emit a fatal error and close the program.
+            match &e {
+                TrySendError::Closed(_) => {
+                    error!("Send-Frame Channel Closed");
+                }
+                TrySendError::Full(_) => {
+                    error!("Send-Frame Buffer Full");
+                }
+            }
+            return Err(e);
+        }
+    }
+    Ok(())
+}
+
+// The following functions control the kafka producer thread
+fn create_producer_task(
+    use_otel: bool,
+    send_frame_buffer_size: usize,
+    producer: &FutureProducer,
+    output_topic: &str,
+) -> AggregatedFrameToBufferSender {
+    let (channel_send, channel_recv) =
+        tokio::sync::mpsc::channel::<AggregatedFrame<EventData>>(send_frame_buffer_size);
+
+    tokio::spawn(produce_to_kafka(
+        use_otel,
+        channel_recv,
+        producer.to_owned(),
+        output_topic.to_owned(),
+    ));
+    channel_send
+}
+
+async fn produce_to_kafka(
+    use_otel: bool,
+    mut channel_recv: Receiver<AggregatedFrame<EventData>>,
+    producer: FutureProducer,
+    output_topic: String,
+) {
+    loop {
+        // Blocks until a frame is received
+        match channel_recv.recv().await {
+            Some(frame) => {
+                produce_frame_to_kafka(use_otel, frame, &producer, &output_topic).await;
+            }
+            None => {
+                error!("Send-Frame Receiver Error");
+                return;
+            }
+        }
+    }
+}
+
+async fn produce_frame_to_kafka(
+    use_otel: bool,
+    frame: AggregatedFrame<EventData>,
     producer: &FutureProducer,
     output_topic: &str,
 ) {
-    if let Some(frame) = cache.poll() {
-        let span = info_span!(target: "otel", "Frame Complete");
-        let future = span.in_scope(|| {
-            let frame_span = frame.span().get().expect("Span should exist").clone();
-            let data: Vec<u8> = frame.into();
+    let frame_span = frame.span().get().expect("Span should exist").clone();
+    let data: Vec<u8> = frame.into();
 
-            let producer = producer.to_owned();
-            let output_topic = output_topic.to_owned();
-            async move {
-                let future_record = FutureRecord::to(&output_topic)
-                    .payload(data.as_slice())
-                    .conditional_inject_span_into_headers(use_otel, &frame_span)
-                    .key("Frame Events List");
+    let future_record = FutureRecord::to(output_topic)
+        .payload(data.as_slice())
+        .conditional_inject_span_into_headers(use_otel, &frame_span)
+        .key("Frame Events List");
 
-                match producer
-                    .send(future_record, Timeout::After(Duration::from_millis(100)))
-                    .await
-                {
-                    Ok(r) => {
-                        debug!("Delivery: {:?}", r);
-                        counter!(FRAMES_SENT).increment(1)
-                    }
-                    Err(e) => {
-                        error!("Delivery failed: {:?}", e);
-                        counter!(
-                            FAILURES,
-                            &[failures::get_label(FailureKind::KafkaPublishFailed)]
-                        )
-                        .increment(1);
-                    }
-                }
-            }
-        });
-        kafka_producer_thread_set.spawn(
-            future.instrument(info_span!(target: "otel", parent: &span, "Message Producer")),
-        );
+    match producer.send(future_record, PRODUCER_TIMEOUT).await {
+        Ok(r) => {
+            debug!("Delivery: {:?}", r);
+            counter!(FRAMES_SENT).increment(1)
+        }
+        Err(e) => {
+            error!("Delivery failed: {:?}", e);
+            counter!(
+                FAILURES,
+                &[failures::get_label(FailureKind::KafkaPublishFailed)]
+            )
+            .increment(1);
+        }
     }
 }
