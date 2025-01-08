61ca841cafb5eb559df3dbc17e993d7940d3630f
Dan Nixon <dan@dan-nixon.com>
2025-01-08 10:56:43 +0000

    Propagate digitizer ID list and completion flag to assembled frames

--- digitiser-aggregator/src/frame/aggregated.rs
+++ digitiser-aggregator/src/frame/aggregated.rs
@@ -1,14 +1,15 @@
 use super::partial::PartialFrame;
 use crate::data::{Accumulate, DigitiserData};
-use supermusr_common::spanned::{SpanOnce, Spanned, SpannedMut};
-#[cfg(test)]
-use supermusr_common::DigitizerId;
+use supermusr_common::{
+    spanned::{SpanOnce, Spanned, SpannedMut},
+    DigitizerId,
+};
 use supermusr_streaming_types::FrameMetadata;
 
 pub(crate) struct AggregatedFrame<D> {
     span: SpanOnce,
     pub(crate) metadata: FrameMetadata,
-    #[cfg(test)]
+    pub(crate) complete: bool,
     pub(crate) digitiser_ids: Vec<DigitizerId>,
     pub(crate) digitiser_data: D,
 }
@@ -17,13 +18,14 @@ pub(crate) struct AggregatedFrame<D> {
 impl<D> AggregatedFrame<D> {
     pub(crate) fn new(
         metadata: FrameMetadata,
-        #[cfg(test)] digitiser_ids: Vec<DigitizerId>,
+        complete: bool,
+        digitiser_ids: Vec<DigitizerId>,
         digitiser_data: D,
     ) -> Self {
         Self {
             span: Default::default(),
             metadata,
-            #[cfg(test)]
+            complete,
             digitiser_ids,
             digitiser_data,
         }
@@ -41,7 +43,7 @@ where
                 .take()
                 .expect("partial frame should have a span"),
             metadata: partial.metadata.clone(),
-            #[cfg(test)]
+            complete: partial.is_complete(),
             digitiser_ids: partial.digitiser_ids(),
             digitiser_data: <DigitiserData<D> as Accumulate<D>>::accumulate(
                 &mut partial.digitiser_data,
