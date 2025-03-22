8e1875a3d5138762ace0a0ff0f6a045d1aa0a7df
Modularius <daniel.kirk@rocketmail.com>
2025-03-22 10:57:46 +0000

    Added units handler

---
+++ nexus-writer/src/nexus/units.rs
@@ -0,0 +1,15 @@
+use hdf5::Dataset;
+
+use crate::{hdf5_handlers::NexusHDF5Result, run_engine::{DatasetExt, HasAttributesExt}};
+
+
+pub(crate) trait DatasetUnitExt : DatasetExt {
+    fn with_units(self, units: &str) -> NexusHDF5Result<Dataset>;
+}
+
+impl DatasetUnitExt for Dataset {
+    fn with_units(self, units: &str) -> NexusHDF5Result<Dataset> {
+        self.add_attribute_to("units", units)?;
+        Ok(self)
+    }
+}
