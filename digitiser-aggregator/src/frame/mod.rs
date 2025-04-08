cb32ea6525e34222e075103f388581ab8f01c4c9
Modularius <daniel.kirk@rocketmail.com>
2025-04-08 23:13:43 +0000

    Add RejectMessageError

--- digitiser-aggregator/src/frame/mod.rs
+++ digitiser-aggregator/src/frame/mod.rs
@@ -4,3 +4,17 @@ mod partial;
 
 pub(crate) use aggregated::AggregatedFrame;
 pub(crate) use cache::FrameCache;
+
+pub(crate) enum RejectMessageError {
+    IdAlreadyPresent,
+    TimestampTooEarly,
+}
+
+impl Into<&'static str> for RejectMessageError {
+    fn into(self) -> &'static str {
+        match self {
+            RejectMessageError::IdAlreadyPresent => "id_already_present",
+            RejectMessageError::TimestampTooEarly => "timestamp_too_early",
+        }
+    }
+}
