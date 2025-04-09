b6a7820611d6bb7fc218e61e08689bf95adb2b7f
Modularius <daniel.kirk@rocketmail.com>
2025-04-09 22:44:29 +0000

    Removed obsolete files

--- nexus-writer/src/run_engine/hdf5_file/mod.rs
+++
@@ -1,9 +0,0 @@
-mod error;
-mod hdf5_writer;
-mod run_file;
-mod run_file_components;
-
-pub(crate) use error::NexusHDF5Error;
-//pub(crate) use hdf5_writer::{AttributeExt, DatasetExt, GroupExt, HasAttributesExt};
-pub(crate) use run_file::RunFile;
-use run_file_components::EventRun;
