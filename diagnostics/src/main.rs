0fa1c5e895ffe4d133853f2bde00d338e1447cec
Dan Nixon <dan@dan-nixon.com>
2025-05-22 11:00:29 +0000

    Diagnostics: add ability to decode a flatbuffer message
    Ideally suited to paste in the byte representation from Redpanda
    Console.

--- diagnostics/src/main.rs
+++ diagnostics/src/main.rs
@@ -1,8 +1,18 @@
 mod daq_trace;
-mod message_debug;
+mod flatbuffer_decode;
+mod kafka_tail;
 
 use clap::{Args, Parser, Subcommand};
 use supermusr_common::CommonKafkaOpts;
+use supermusr_streaming_types::{
+    dat2_digitizer_analog_trace_v2_generated::{
+        digitizer_analog_trace_message_buffer_has_identifier,
+        root_as_digitizer_analog_trace_message,
+    },
+    ecs_6s4t_run_stop_generated::{root_as_run_stop, run_stop_buffer_has_identifier},
+    ecs_pl72_run_start_generated::{root_as_run_start, run_start_buffer_has_identifier},
+};
+use tracing::{info, warn};
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -18,8 +28,18 @@ enum Commands {
     DaqTrace(DaqTraceOpts),
 
     /// Run message dumping tool.
-    #[clap(name = "message-debug")]
-    MessageDebug(CommonOpts),
+    #[clap(name = "kafka-tail")]
+    KafkaTail(CommonOpts),
+
+    /// Decode a flatbuffer encoded message.
+    ///
+    /// Shows a basic summary of the following message types:
+    /// Digitiser trace, Run start, Run stop
+    ///
+    /// Encoded message must be provided in hexadecimal representation with a space between each
+    /// byte (as per the format used by Redpanda Console).
+    #[clap(name = "decode")]
+    FlatbufferDecode,
 }
 
 #[derive(Debug, Args)]
@@ -51,6 +71,49 @@ async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     match cli.command {
         Commands::DaqTrace(args) => daq_trace::run(args).await,
-        Commands::MessageDebug(args) => message_debug::run(args).await,
+        Commands::KafkaTail(args) => kafka_tail::run(args).await,
+        Commands::FlatbufferDecode => flatbuffer_decode::run().await,
+    }
+}
+
+fn decode_and_print(payload: &[u8]) {
+    if digitizer_analog_trace_message_buffer_has_identifier(payload) {
+        match root_as_digitizer_analog_trace_message(payload) {
+            Ok(data) => {
+                info!(
+                    "Trace packet: dig. ID: {}, metadata: {:?}",
+                    data.digitizer_id(),
+                    data.metadata()
+                );
+            }
+            Err(e) => {
+                warn!("Failed to parse message: {}", e);
+            }
+        }
+    } else if run_start_buffer_has_identifier(payload) {
+        match root_as_run_start(payload) {
+            Ok(data) => {
+                info!(
+                    "Run start: run name: {:?}, start time: {}",
+                    data.run_name(),
+                    data.start_time()
+                );
+            }
+            Err(e) => {
+                warn!("Failed to parse message: {}", e);
+            }
+        }
+    } else if run_stop_buffer_has_identifier(payload) {
+        match root_as_run_stop(payload) {
+            Ok(data) => {
+                info!("Run stop: run name: {:?}", data.run_name());
+            }
+            Err(e) => {
+                warn!("Failed to parse message: {}", e);
+            }
+        }
+    } else {
+        // TODO: other message types
+        warn!("Unexpected message type");
     }
 }
