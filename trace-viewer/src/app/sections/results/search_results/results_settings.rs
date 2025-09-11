7e20e70008fc6cdc1ac2ef34301fc4ad6ca27f44
Modularius <daniel.kirk@rocketmail.com>
2025-09-11 14:58:00 +0000

    Moved results settings

---
+++ trace-viewer/src/app/sections/results/search_results/results_settings.rs
@@ -0,0 +1,31 @@
+use crate::{app::sections::results::{context::ResultsLevelContext, search_results::SelectTraceLevelContext}, structs::SearchTargetBy};
+use leptos::{component, either::Either, prelude::*, view, IntoView};
+
+#[component]
+pub(crate) fn ResultsSettingsPanel() -> impl IntoView {
+    let target = use_context::<SelectTraceLevelContext>().expect("").target;
+    
+    view!{
+        <div class = "search-results-settings">
+            <ShowSelectedChannelsOnly by = target.by />
+        </div>
+    }
+}
+
+#[component]
+pub(crate) fn ShowSelectedChannelsOnly(by: SearchTargetBy) -> impl IntoView {
+    let result_level_context = use_context::<ResultsLevelContext>()
+        .expect("results_settings_node_refs should be provided, this should never fail.");
+
+    match by {
+        SearchTargetBy::ByChannels { channels: _ } => Either::Left(view! {
+            <label class = "results-settings-input" for = "selected-channels-only">
+                "Selected channels only:"
+                <input class = "results-settings-input" name = "selected-channels-only" id = "selected-channels-only" type = "checkbox"
+                    bind:value = result_level_context.selected_channels_only
+                />
+            </label>
+        }),
+        _ => Either::Right(()),
+    }
+}
