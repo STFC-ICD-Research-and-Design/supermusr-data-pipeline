3c90418d8a2d2591224a9a87238033fa740baf2b
Modularius <daniel.kirk@rocketmail.com>
2025-10-03 14:19:40 +0000

    Add handling for array run log types

--- nexus-writer/src/error.rs
+++ nexus-writer/src/error.rs
@@ -136,4 +136,7 @@ pub(crate) enum FlatBufferMissingError {
     /// The file name string is missing.
     #[error("File name missing from flatbuffer RunStart Message")]
     FileName,
+    /// Array value missing from a log.
+    #[error("Array missing from a log.")]
+    Array,
 }
