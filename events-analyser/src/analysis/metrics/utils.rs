a068be5b3747f7e98529dc2301c2146bdbec24a3
Modularius <daniel.kirk@rocketmail.com>
2026-07-22 18:46:03 +0000

    Record extreme histogram values in fields

--- events-analyser/src/analysis/metrics/utils.rs
+++ events-analyser/src/analysis/metrics/utils.rs
@@ -257,6 +257,8 @@ pub(crate) struct Histogram {
     bins: Vec<f64>,
     bin_labels: Vec<f64>,
     interval: Interval<f64>,
+    num_too_small: usize,
+    num_too_big: usize,
 }
 
 impl Histogram {
@@ -269,49 +271,36 @@ impl Histogram {
             bin_labels,
             bins,
             interval: interval.clone(),
+            num_too_small: Default::default(),
+            num_too_big: Default::default(),
         }
     }
 
     pub(crate) fn push(&mut self, value: f64) {
-        self.num_values += 1;
         if self.interval.min <= value && value < self.interval.max {
+            self.num_values += 1;
             let index = (self.bins.len() as f64 * (value - self.interval.min)
                 / (self.interval.max - self.interval.min)) as usize;
             self.bins
                 .get_mut(index)
                 .expect("Element should exist, this should never fail")
                 .add_assign(1.0);
+        } else if value < self.interval.min {
+            self.num_too_small += 1;
         } else {
-            warn!(
-                "Histogram value out of range {value} \notin ({},{})",
-                self.interval.min, self.interval.max
-            );
+            self.num_too_big += 1;
         }
     }
 
     pub(crate) fn get_bin_labels(&self) -> &[f64] {
         &self.bin_labels
     }
-    /*
-        pub(crate) fn normalise(&mut self) {
-            for bin in &mut self.bins {
-                bin.div_assign(self.num_values as f64)
-            }
-        }
-    */
+
     pub(crate) fn get_normalised_counts(&self) -> Vec<f64> {
         let coef = 1.0 / self.num_values as f64;
         self.bins.iter().map(|value| value * coef).collect()
     }
-    /*
-    pub(crate) fn get_counts(&self) -> &[f64] {
-        &self.bins
-    }
 
-    pub(crate) fn get_num_values(&self) -> usize {
-        self.num_values
-    }
-    */
     #[cfg(test)]
     pub(crate) fn set(&mut self, bins: Vec<f64>) {
         self.num_values = bins.iter().sum::<f64>() as usize;
